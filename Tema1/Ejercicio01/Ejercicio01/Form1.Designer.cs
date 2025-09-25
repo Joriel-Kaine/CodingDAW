eb505c824bc7a746cb82250e2b48615fd12c9de6
Manuel MR <manuel.monred@gmail.com>
2025-09-25 20:44:18 +0200

    Primera tarea
    Eventos de caja al presionar los botones

---
+++ Tema1/Ejercicio01/Ejercicio01/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Ejercicio01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrimero = new System.Windows.Forms.Button();
+            this.btnSegundo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPrimero
+            // 
+            this.btnPrimero.BackColor = System.Drawing.Color.Wheat;
+            this.btnPrimero.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnPrimero.FlatAppearance.BorderSize = 4;
+            this.btnPrimero.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrimero.Font = new System.Drawing.Font("Lato", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPrimero.Location = new System.Drawing.Point(991, 377);
+            this.btnPrimero.Name = "btnPrimero";
+            this.btnPrimero.Size = new System.Drawing.Size(280, 120);
+            this.btnPrimero.TabIndex = 0;
+            this.btnPrimero.Text = "Primer Botón";
+            this.btnPrimero.UseVisualStyleBackColor = false;
+            this.btnPrimero.Click += new System.EventHandler(this.btnPrimero_Click);
+            // 
+            // btnSegundo
+            // 
+            this.btnSegundo.BackColor = System.Drawing.Color.DarkKhaki;
+            this.btnSegundo.FlatAppearance.BorderColor = System.Drawing.Color.Silver;
+            this.btnSegundo.FlatAppearance.BorderSize = 4;
+            this.btnSegundo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSegundo.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSegundo.Location = new System.Drawing.Point(1042, 562);
+            this.btnSegundo.Name = "btnSegundo";
+            this.btnSegundo.Size = new System.Drawing.Size(180, 100);
+            this.btnSegundo.TabIndex = 1;
+            this.btnSegundo.Text = "Segundo Botón";
+            this.btnSegundo.UseVisualStyleBackColor = false;
+            this.btnSegundo.Click += new System.EventHandler(this.btnSegundo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Ejercicio01.Properties.Resources.Teddy_Bear;
+            this.ClientSize = new System.Drawing.Size(1344, 712);
+            this.Controls.Add(this.btnSegundo);
+            this.Controls.Add(this.btnPrimero);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrimero;
+        private System.Windows.Forms.Button btnSegundo;
+    }
+}
+
