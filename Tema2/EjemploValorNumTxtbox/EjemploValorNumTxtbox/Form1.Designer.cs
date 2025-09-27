f334a08ce7c8dce7a95076524a3cefc639973411
Manuel MR <manuel.monred@gmail.com>
2025-09-28 00:35:10 +0200

    Tema 2 - Ejemplo04
    Ejemplo donde hemos obtenido un valor número a través de un textbox.

---
+++ Tema2/EjemploValorNumTxtbox/EjemploValorNumTxtbox/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace EjemploValorNumTxtbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnValor = new System.Windows.Forms.Button();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnValor
+            // 
+            this.btnValor.BackColor = System.Drawing.Color.Wheat;
+            this.btnValor.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnValor.FlatAppearance.BorderSize = 2;
+            this.btnValor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnValor.Font = new System.Drawing.Font("Lato", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnValor.Location = new System.Drawing.Point(228, 384);
+            this.btnValor.Name = "btnValor";
+            this.btnValor.Size = new System.Drawing.Size(300, 60);
+            this.btnValor.TabIndex = 0;
+            this.btnValor.Text = "Valor";
+            this.btnValor.UseVisualStyleBackColor = false;
+            this.btnValor.Click += new System.EventHandler(this.btnValor_Click);
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Lato", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(143, 185);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(142, 39);
+            this.lblNumero.TabIndex = 1;
+            this.lblNumero.Text = "Número:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Lato", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(291, 185);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(316, 46);
+            this.txtNumero.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(778, 744);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.btnValor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnValor;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox txtNumero;
+    }
+}
+
---
+++ Tema2/EjemploValorNumTxtbox/EjemploValorNumTxtbox/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploValorNumTxtbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValor_Click(object sender, EventArgs e)
+        {
+            // Declaración de variable.
+            int numero;
+
+            // Asignación de valor a la variable.
+            numero = int.Parse(txtNumero.Text);
+
+            // Mostrar valor de la variable en un MessageBox.
+            MessageBox.Show(numero.ToString());
+
+        }
+    }
+}
