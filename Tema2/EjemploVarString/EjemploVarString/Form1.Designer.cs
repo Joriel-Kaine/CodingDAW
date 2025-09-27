7e8de4dc642bf328cd7693a4de48b68ead9f3394
Manuel MR <manuel.monred@gmail.com>
2025-09-27 16:37:27 +0200

    Tema 2 - Ejemplo 1
    Ejemplo con variable tipo String.

---
+++ Tema2/EjemploVarString/EjemploVarString/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace EjemploVarString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.btnNombre = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(152, 168);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(107, 29);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(152, 288);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(302, 165);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(289, 36);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApellido.Location = new System.Drawing.Point(302, 285);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(289, 36);
+            this.txtApellido.TabIndex = 3;
+            // 
+            // btnNombre
+            // 
+            this.btnNombre.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.btnNombre.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.btnNombre.FlatAppearance.BorderSize = 2;
+            this.btnNombre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNombre.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNombre.Location = new System.Drawing.Point(157, 435);
+            this.btnNombre.Name = "btnNombre";
+            this.btnNombre.Size = new System.Drawing.Size(434, 60);
+            this.btnNombre.TabIndex = 4;
+            this.btnNombre.Text = "Mostrar nombre completo";
+            this.btnNombre.UseVisualStyleBackColor = false;
+            this.btnNombre.Click += new System.EventHandler(this.btnNombre_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(778, 744);
+            this.Controls.Add(this.btnNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.Button btnNombre;
+    }
+}
+
---
+++ Tema2/EjemploVarString/EjemploVarString/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploVarString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNombre_Click(object sender, EventArgs e)
+        {
+            string nombre, apellido;
+            string nombreCompleto;
+
+            // Aquí recogemos el nombre del textbox correspondiente
+            nombre = txtNombre.Text;
+            // Aquí recogemos el apellido
+            apellido = txtApellido.Text;
+
+            // Aquí montamos la frase final, utilizando la concatenación
+            nombreCompleto = "Tu nombre completo es: " + nombre + " " + apellido;
+
+            MessageBox.Show(nombreCompleto);
+        }
+    }
+}
