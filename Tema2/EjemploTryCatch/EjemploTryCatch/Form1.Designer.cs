0133dcfab67d5036fbdeee96a35a9379b30c3c47
Manuel MR <manuel.monred@gmail.com>
2025-09-28 02:41:57 +0200

    Tema 2 - Ejemplo 5
    Ejemplo usando el TRY y CATCH para los errores.

---
+++ Tema2/EjemploTryCatch/EjemploTryCatch/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace EjemploTryCatch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDividendo = new System.Windows.Forms.Label();
+            this.lblDivisor = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtDividendo = new System.Windows.Forms.TextBox();
+            this.txtDivisor = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDividendo
+            // 
+            this.lblDividendo.AutoSize = true;
+            this.lblDividendo.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDividendo.Location = new System.Drawing.Point(66, 189);
+            this.lblDividendo.Name = "lblDividendo";
+            this.lblDividendo.Size = new System.Drawing.Size(131, 29);
+            this.lblDividendo.TabIndex = 0;
+            this.lblDividendo.Text = "Dividendo:";
+            // 
+            // lblDivisor
+            // 
+            this.lblDivisor.AutoSize = true;
+            this.lblDivisor.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDivisor.Location = new System.Drawing.Point(66, 337);
+            this.lblDivisor.Name = "lblDivisor";
+            this.lblDivisor.Size = new System.Drawing.Size(96, 29);
+            this.lblDivisor.TabIndex = 1;
+            this.lblDivisor.Text = "Divisor:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(66, 484);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(126, 29);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // txtDividendo
+            // 
+            this.txtDividendo.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDividendo.Location = new System.Drawing.Point(203, 186);
+            this.txtDividendo.Name = "txtDividendo";
+            this.txtDividendo.Size = new System.Drawing.Size(232, 36);
+            this.txtDividendo.TabIndex = 3;
+            // 
+            // txtDivisor
+            // 
+            this.txtDivisor.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDivisor.Location = new System.Drawing.Point(203, 334);
+            this.txtDivisor.Name = "txtDivisor";
+            this.txtDivisor.Size = new System.Drawing.Size(232, 36);
+            this.txtDivisor.TabIndex = 4;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(203, 481);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(232, 36);
+            this.txtResultado.TabIndex = 5;
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.BackColor = System.Drawing.Color.Wheat;
+            this.btnDividir.FlatAppearance.BorderColor = System.Drawing.Color.Gray;
+            this.btnDividir.FlatAppearance.BorderSize = 2;
+            this.btnDividir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDividir.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDividir.Location = new System.Drawing.Point(501, 326);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(199, 58);
+            this.btnDividir.TabIndex = 6;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = false;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(778, 744);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtDivisor);
+            this.Controls.Add(this.txtDividendo);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblDivisor);
+            this.Controls.Add(this.lblDividendo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDividendo;
+        private System.Windows.Forms.Label lblDivisor;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtDividendo;
+        private System.Windows.Forms.TextBox txtDivisor;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Button btnDividir;
+    }
+}
+
---
+++ Tema2/EjemploTryCatch/EjemploTryCatch/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploTryCatch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDividir_Click (object sender, EventArgs e)
+        {
+            int dividendo, divisor;
+            int resultado;
+
+            try
+            {
+                dividendo = int.Parse(txtDividendo.Text);
+                divisor = int.Parse(txtDivisor.Text);
+
+                resultado = dividendo / divisor;
+
+                txtResultado.Text = resultado.ToString();
+            }
+            catch (FormatException fEx) 
+            {
+                MessageBox.Show ("Se ha producido el error: " + fEx.Message);
+            }
+            catch (DivideByZeroException oEx)
+            {
+                MessageBox.Show (oEx.Message);
+            }
+
+
+        }
+    }
+}
