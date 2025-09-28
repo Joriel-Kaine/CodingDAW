df120bbdbb816e2d03e7763268de86973dd6a9eb
Manuel MR <manuel.monred@gmail.com>
2025-09-28 19:46:09 +0200

    Tema 2 - Tarea 1
    Lo escrito en el PDF llevado a código en Visual Studio.

---
+++ Tema2/Ejercicio01/Ejercicio01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Salida en consola.
+            Console.WriteLine("Ejercicio 1");
+            Console.WriteLine("Identificadores y Tipos");
+            Console.WriteLine("=============");
+
+            // Mostrar mensajes en consola de distintos tipos.
+            int edad = Utils.PedirEdad();
+            Console.WriteLine("Tu edad es de " + edad + " años.");
+
+            string codPostal = Utils.PedirCodPostal();
+            Console.WriteLine("Tu código postal es " + codPostal + ".");
+
+            string dni = Utils.PedirDni();
+            Console.WriteLine("Tu DNI es " + dni + ".");
+
+            double altura = Utils.PedirAltura();
+            Console.WriteLine("Tu altura es de " + altura + " centímetros.");
+
+            char genero = Utils.PedirGenero();
+            Console.WriteLine("Tu género es " + genero + ".");
+
+            bool estadoCivil = Utils.EstarCasado();
+            Console.WriteLine("¿Estás casado? " + estadoCivil + ".");
+
+            Console.Write("Pulsa una tecla para finalizar...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tema2/Ejercicio01/Ejercicio01/Utils.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio01
+{
+    public class Utils
+    {
+        public static int PedirEdad()
+        {
+            // Pedir al usuario que introduzca su edad.
+            Console.Write("Por favor, introduce tu edad: ");
+
+            // Leer la entrada del usuario.
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static string PedirCodPostal()
+        {
+            // Pedir al usuario que introduzca su código postal.
+            Console.Write("Por favor, introduce tu código postal: ");
+
+            // Leer la entrada del usuario.
+            return Console.ReadLine();
+        }
+
+        public static string PedirDni()
+        {
+            // Pedir al usuario que introduzca su DNI.
+            Console.Write("Por favor, introduce tu DNI: ");
+
+            // Leer la entrada del usuario.
+            return Console.ReadLine();
+        }
+
+        public static double PedirAltura()
+        {
+            // Pedir al usuario que introduzca su altura.
+            Console.Write("Por favor, introduce tu altura en metros: ");
+
+            // Leer la entrada del usuario.
+            return double.Parse(Console.ReadLine());
+        }
+
+        public static char PedirGenero()
+        {
+            // Pedir al usuario que introduzca su género.
+            Console.Write("Por favor, introduce tu género (M/F/X): ");
+
+            // Leer la entrada del usuario.
+            return char.Parse(Console.ReadLine());
+        }
+
+        public static bool EstarCasado()
+        {
+            // Pedir al usuario que diga si está casado.
+            Console.Write("¿Estás casado? (S/N): ");
+
+            // Leer la entrada del usuario.
+            string estadoCivil = Console.ReadLine();
+            if (estadoCivil == "S" || estadoCivil == "s")
+                return true;
+            return false;
+        }
+
+        public static decimal IntroducirIva()
+        {
+            // Pedir al usuario que introduzca el IVA.
+            Console.Write("Por favor, introduce el IVA (en decimales, no porcentaje): ");
+
+            // Leer la entrada del usuario.
+            return decimal.Parse(Console.ReadLine());
+        }
+
+        public enum Transporte
+        {
+            Coche,
+            Tren,
+            Avión,
+            Barco
+        }
+
+        public static Transporte PedirTransporte()
+        {
+            return Transporte.Coche;
+        }
+    }
+}
