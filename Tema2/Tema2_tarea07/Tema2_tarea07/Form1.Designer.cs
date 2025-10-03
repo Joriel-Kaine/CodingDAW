7235808d02e9c00cf572d939e63434fd1d4109d1
Manuel MR <manuel.monred@gmail.com>
2025-10-03 17:05:44 +0200

    Tema 2
    Tarea 7. Salida de cociente de una división. Uso de try / catch / finally.

---
+++ Tema2/Tema2_tarea07/Tema2_tarea07/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Tema2_tarea07
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblDividendo = new System.Windows.Forms.Label();
+            this.lblDivisor = new System.Windows.Forms.Label();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.lblCociente = new System.Windows.Forms.Label();
+            this.txtDividendo = new System.Windows.Forms.TextBox();
+            this.txtDivisor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lato", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ejercicio 7";
+            // 
+            // lblDividendo
+            // 
+            this.lblDividendo.AutoSize = true;
+            this.lblDividendo.Font = new System.Drawing.Font("Lato", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDividendo.Location = new System.Drawing.Point(120, 214);
+            this.lblDividendo.Name = "lblDividendo";
+            this.lblDividendo.Size = new System.Drawing.Size(151, 34);
+            this.lblDividendo.TabIndex = 1;
+            this.lblDividendo.Text = "Dividendo:";
+            // 
+            // lblDivisor
+            // 
+            this.lblDivisor.AutoSize = true;
+            this.lblDivisor.Font = new System.Drawing.Font("Lato", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDivisor.Location = new System.Drawing.Point(120, 307);
+            this.lblDivisor.Name = "lblDivisor";
+            this.lblDivisor.Size = new System.Drawing.Size(110, 34);
+            this.lblDivisor.TabIndex = 2;
+            this.lblDivisor.Text = "Divisor:";
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.BackColor = System.Drawing.Color.Wheat;
+            this.btnDividir.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.btnDividir.FlatAppearance.BorderSize = 2;
+            this.btnDividir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDividir.Font = new System.Drawing.Font("Lato", 26F, System.Drawing.FontStyle.Bold);
+            this.btnDividir.Location = new System.Drawing.Point(219, 436);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(300, 100);
+            this.btnDividir.TabIndex = 3;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = false;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // lblCociente
+            // 
+            this.lblCociente.AutoSize = true;
+            this.lblCociente.Font = new System.Drawing.Font("Lato", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCociente.Location = new System.Drawing.Point(118, 601);
+            this.lblCociente.Name = "lblCociente";
+            this.lblCociente.Size = new System.Drawing.Size(188, 48);
+            this.lblCociente.TabIndex = 4;
+            this.lblCociente.Text = "Cociente:";
+            // 
+            // txtDividendo
+            // 
+            this.txtDividendo.Font = new System.Drawing.Font("Lato Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDividendo.Location = new System.Drawing.Point(293, 211);
+            this.txtDividendo.Name = "txtDividendo";
+            this.txtDividendo.Size = new System.Drawing.Size(226, 41);
+            this.txtDividendo.TabIndex = 5;
+            // 
+            // txtDivisor
+            // 
+            this.txtDivisor.Font = new System.Drawing.Font("Lato Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDivisor.Location = new System.Drawing.Point(293, 304);
+            this.txtDivisor.Name = "txtDivisor";
+            this.txtDivisor.Size = new System.Drawing.Size(226, 41);
+            this.txtDivisor.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(778, 744);
+            this.Controls.Add(this.txtDivisor);
+            this.Controls.Add(this.txtDividendo);
+            this.Controls.Add(this.lblCociente);
+            this.Controls.Add(this.btnDividir);
+            this.Controls.Add(this.lblDivisor);
+            this.Controls.Add(this.lblDividendo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblDividendo;
+        private System.Windows.Forms.Label lblDivisor;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Label lblCociente;
+        private System.Windows.Forms.TextBox txtDividendo;
+        private System.Windows.Forms.TextBox txtDivisor;
+    }
+}
+
---
+++ Tema2/Tema2_tarea07/Tema2_tarea07/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema2_tarea07
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int dividendo = int.Parse(txtDividendo.Text);
+                int divisor = int.Parse(txtDivisor.Text);
+
+                int cociente = dividendo / divisor;
+                lblCociente.Text = ($"Cociente: {cociente}");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Debes introducir valores numéricos.", "Error de formato");
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Intento de división por cero.", "Error de divisón por 0");
+            }
+            finally
+            {
+                txtDividendo.Clear();
+                txtDivisor.Clear();
+            }
+        }
+    }
+}
