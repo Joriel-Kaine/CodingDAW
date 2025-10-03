e5823ec0f2a23717a1f378965dd97dff12702f33
Manuel MR <manuel.monred@gmail.com>
2025-10-03 03:25:32 +0200

    Tema 2
    Tarea 6. Añadido Try / Catch a la aplicación.

--- Tema2/Tema2_Tarea06/Tema2_Tarea06/Form1.cs
+++ Tema2/Tema2_Tarea06/Tema2_Tarea06/Form1.cs
@@ -19,29 +19,37 @@ namespace Tema2_Tarea06
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Declaramos las dos variables.
-            int num1 = int.Parse(txtNum1.Text);
-            int num2 = int.Parse(txtNum2.Text);
+            try
+            {
+                // Declaramos las dos variables.
+                int num1 = int.Parse(txtNum1.Text);
+                int num2 = int.Parse(txtNum2.Text);
 
-            // Realizamos la suma.
-            int suma = num1 + num2;
-            lblSuma.Text = ($"Suma:  {suma}");
+                // Realizamos la suma.
+                int suma = num1 + num2;
+                lblSuma.Text = ($"Suma:  {suma}");
 
-            // Realizamos la resta.
-            int resta = num1 - num2;
-            lblResta.Text = ($"Resta:  {resta}");
+                // Realizamos la resta.
+                int resta = num1 - num2;
+                lblResta.Text = ($"Resta:  {resta}");
 
-            // Realizamos el producto.
-            int producto = num1 * num2;
-            lblProducto.Text = ($"Producto:  {producto}");
+                // Realizamos el producto.
+                int producto = num1 * num2;
+                lblProducto.Text = ($"Producto:  {producto}");
 
-            // Realizamos una división entera.
-            int division = num1 / num2;
-            lblDivision.Text = ($"División entera:  {division}");
+                // Realizamos una división entera.
+                int division = num1 / num2;
+                lblDivision.Text = ($"División entera:  {division}");
 
-            // Realizamos el resto.
-            int resto = num1 % num2;
-            lblResto.Text = ($"Resto:  {resto}");
+                // Realizamos el resto.
+                int resto = num1 % num2;
+                lblResto.Text = ($"Resto:  {resto}");
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Has introducido un valor no numérico en Num1 o Num2.\nPor favor introduce un valor correcto.", "Error de formato");
+            }
+
         }
     }
 }
