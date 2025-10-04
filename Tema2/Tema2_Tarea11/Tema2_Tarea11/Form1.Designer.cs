d4e5d438b41334f1ec81786989af3ebdaf85190d
Manuel MR <manuel.monred@gmail.com>
2025-10-04 16:52:58 +0200

    Tema 2
    Tarea 11. Poner la altura de una persona en metros y centímetros por separado.

---
+++ Tema2/Tema2_Tarea11/Tema2_Tarea11/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Tema2_Tarea11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.lblCms = new System.Windows.Forms.Label();
+            this.btnResultado = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(62, 49);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(86, 29);
+            this.lblAltura.TabIndex = 0;
+            this.lblAltura.Text = "Altura:";
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Font = new System.Drawing.Font("Lato Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAltura.Location = new System.Drawing.Point(166, 46);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(240, 36);
+            this.txtAltura.TabIndex = 1;
+            // 
+            // lblCms
+            // 
+            this.lblCms.AutoSize = true;
+            this.lblCms.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCms.Location = new System.Drawing.Point(412, 49);
+            this.lblCms.Name = "lblCms";
+            this.lblCms.Size = new System.Drawing.Size(56, 29);
+            this.lblCms.TabIndex = 2;
+            this.lblCms.Text = "cms";
+            // 
+            // btnResultado
+            // 
+            this.btnResultado.BackColor = System.Drawing.Color.Wheat;
+            this.btnResultado.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.btnResultado.FlatAppearance.BorderSize = 2;
+            this.btnResultado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnResultado.Font = new System.Drawing.Font("Lato", 16F, System.Drawing.FontStyle.Bold);
+            this.btnResultado.Location = new System.Drawing.Point(436, 147);
+            this.btnResultado.Name = "btnResultado";
+            this.btnResultado.Size = new System.Drawing.Size(200, 60);
+            this.btnResultado.TabIndex = 3;
+            this.btnResultado.Text = "Resultado";
+            this.btnResultado.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 344);
+            this.Controls.Add(this.btnResultado);
+            this.Controls.Add(this.lblCms);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.lblAltura);
+            this.Name = "Form1";
+            this.Text = "Ejercicio 11";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label lblCms;
+        private System.Windows.Forms.Button btnResultado;
+    }
+}
+
