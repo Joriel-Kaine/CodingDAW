07bdf1964593ca820f98d968608c4360025f04f1
Manuel MR <manuel.monred@gmail.com>
2025-10-04 14:27:36 +0200

    Tema 2
    Tarea 10. Ejercicio hecho en papel llevado a código para comprobación.

---
+++ Tema2/Tema2_Tarea10/Tema2_Tarea10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema2_Tarea10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2 + 3 * 5); // Resultado: 17.
+
+            Console.WriteLine((2 + 3) * 5); // Resultado: 25.
+
+            Console.WriteLine((5 + 2) == 7 && (3 > 1)); // Resultado: true.
+
+            Console.WriteLine((5 + 2) == 7 || (3 < 1)); // Resultado: true.
+
+            Console.WriteLine((!true) || (false && true)); // Resultado: false.
+
+            Console.WriteLine(10 / 3); // Resultado: 3.
+
+            Console.WriteLine((1 + 2 * 3) == 7 || (4 % 2) == 0); // Resultado: true.
+        }
+    }
+}
