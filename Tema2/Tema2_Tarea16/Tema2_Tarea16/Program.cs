1cd814ccd7b7949432a3aef0c7abefe015f822df
Manuel MR <manuel.monred@gmail.com>
2025-10-06 17:15:27 +0200

    Tema 2
    Tarea 16. Uso de importes en decimal y retención en una nómina.

---
+++ Tema2/Tema2_Tarea16/Tema2_Tarea16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema2_Tarea16
+{
+    internal class Program
+    {
+        const decimal retencion = 18;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tarea 16: Manuel MR");
+
+            Console.Write("\nEscribe el número de horas normales al mes: ");
+            int horasNormal = int.Parse(Console.ReadLine());
+
+            Console.Write("Escribe el número de horas extra al mes: ");
+            int horasExtra = int.Parse(Console.ReadLine());
+
+            Console.Write("Escribe el precio por hora: ");
+            decimal precioHora = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nHoras normales: {horasNormal}, horas extra: {horasExtra} y un precio/hora de {precioHora} euros.");
+
+            decimal nominaSinRetencion = (horasNormal * precioHora) + (horasExtra * (precioHora * 2));
+            decimal nominaMensual = nominaSinRetencion - (nominaSinRetencion * (retencion / 100));
+
+            Console.WriteLine($"\nNómina mensual sin retención: {nominaSinRetencion} euros.");
+            Console.WriteLine($"Nómina mensual: {nominaMensual} euros.");
+        }
+    }
+}
