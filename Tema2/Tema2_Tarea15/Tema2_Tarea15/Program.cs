07377cd7cc64f9fda16b1bdd8b91e73eddbd2bdd
Manuel MR <manuel.monred@gmail.com>
2025-10-06 04:11:44 +0200

    Tema 2
    Tarea 15. Sacado capital final añadiendo el interés a una cantidad ingresada.

---
+++ Tema2/Tema2_Tarea15/Tema2_Tarea15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema2_Tarea15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tarea 15: Manuel MR");
+
+            Console.Write("\nEscribe el importe a ingresar del cliente: ");
+            decimal cuenta = decimal.Parse(Console.ReadLine());
+
+            Console.Write("\nEscribe el interés anual: ");
+            decimal interes = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine($"\nEl cliente tiene {cuenta} euros en su cuenta y el interés anual es del {interes}%.");
+
+            decimal capital = cuenta + (cuenta * (interes / 100));
+            Console.WriteLine($"\nCapital final: {capital} euros.");
+        }
+    }
+}
