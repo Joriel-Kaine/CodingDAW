c9490ada8f4645ac57dd0ee0c13846fc688c0699
Manuel MR <manuel.monred@gmail.com>
2025-10-06 03:18:37 +0200

    Tema 2
    Tarea 14. Cálculos con precios e IVA en consola.

---
+++ Tema2/Tema2_Tarea14/Tema2_Tarea14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema2_Tarea14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Tarea 14: Manuel MR\n");
+
+            const decimal IVA = 0.21m;
+
+            Console.Write("Introduce el precio del primer producto: ");
+            decimal productoA = decimal.Parse(Console.ReadLine());
+
+            Console.Write("\nIntroduce el precio del segundo producto: ");
+            decimal productoB = decimal.Parse(Console.ReadLine());
+
+            Console.Write("\nIntroduce el precio del tercer producto: ");
+            decimal productoC = decimal.Parse(Console.ReadLine());
+
+            // Sacamos por consola los precios introducidos.
+            Console.WriteLine($"\nEl precio de los productos es: {productoA}€, {productoB}€ y {productoC}€.");
+
+            // Calculamos el total sin IVA.
+            decimal totalSinIVA = productoA + productoB + productoC;
+            Console.WriteLine($"\nPrecio total sin IVA: {totalSinIVA}");
+
+            // Calculamos el total con IVA.
+            decimal totalConIVA = totalSinIVA * (1 + IVA); // También se puede hacer "totalSinIVA + (totalSinIVA * IVA)".
+            Console.WriteLine($"Precio total con IVA: {totalConIVA}");
+        }
+    }
+}
