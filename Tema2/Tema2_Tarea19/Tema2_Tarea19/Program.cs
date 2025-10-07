04cc179cd4d9f65ac269d0cd9c3739575e66f79b
Manuel MR <manuel.monred@gmail.com>
2025-10-07 02:44:24 +0200

    Tema 2
    Tarea 20. Incompleto.

---
+++ Tema2/Tema2_Tarea19/Tema2_Tarea19/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema2_Tarea19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            try
+            {
+                Console.WriteLine("Tema 2 - Tarea 19: Manuel MR");
+
+                Console.Write("\nIntroduce un número decimal: ");
+                decimal numero = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine($"\nN0: {numero:N0}" +
+                                  $"\nN2: {numero:N2}" +
+                                  $"\nC2: {numero:C2}" +
+                                  $"\nP2: {numero:P2}");
+
+                Console.WriteLine("\nPulsa una tecla para salir...");
+                Console.ReadKey();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Introduce un valor numérico válido.");
+            }
+        }
+    }
+}
---
+++ Tema2/Tema2_Tarea20/Tema2_Tarea20/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema2_Tarea20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Tema 2 - Tarea 20: Manuel MR");
+
+                Console.Write("\nIntroduce un carácter: ");
+                char caracter = char.Parse(Console.ReadLine());
+
+                bool vocal = "a" == caracter;
+                bool digito = (0 <= caracter) && (caracter <= 9);
+
+                Console.WriteLine($"\n¿Es vocal?: {vocal}" +
+                                  $"\n¿Es dígito?: {digito}");
+
+                Console.WriteLine("\nPulsa una tecla para salir...");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
