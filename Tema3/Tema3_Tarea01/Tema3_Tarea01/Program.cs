0b5c31bf6113a6734b267bbf47d1abb9ee88fc67
Manuel MR <manuel.monred@gmail.com>
2025-10-11 03:55:28 +0200

    Tema 3
    Tarea 01. Uso de IF - ELSE.

--- Tema3/Tema3_Tarea01/Tema3_Tarea01/Program.cs
+++ Tema3/Tema3_Tarea01/Tema3_Tarea01/Program.cs
@@ -10,7 +10,27 @@ namespace Tema3_Tarea01
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine("\nTema 3 - Tarea 01: Manuel MR\n\n");
 
+                Console.Write("Introduce un número entero cualquiera: ");
+                int numEntero = int.Parse(Console.ReadLine());
+
+                if (numEntero == 2)
+                {
+                    Console.WriteLine("\n\nEl número introducido es el 2\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("\n\nEl número introducido NO es el 2\n\n");
+                }
+            }
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("\n\n\n\nIntroduce un valor numérico válido.\n\n\n\n");
+            }
         }
     }
 }
