59efb8545a73ea3a4759f67bfffb32a681c8de6a
Manuel MR <manuel.monred@gmail.com>
2025-10-12 18:41:53 +0200

    Tema 3
    Tarea 2. Uso de IF para comprobar si dos números son iguales.

---
+++ Tema3/Tema3_Tarea02/Tema3_Tarea02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3_Tarea02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 3 - Tarea 2: Manuel MR\n");
+
+            try
+            {
+                // Pedimos para introducir los dos números enteros.
+                Console.Write("Introduce el primer número: ");
+                int primerNum = int.Parse(Console.ReadLine());
+
+                Console.Write("\nIntroduce el segundo número: ");
+                int segundoNum = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+                if (primerNum == segundoNum)
+                {
+                    Console.WriteLine($"\n\n\nLos numeros introducidos ({primerNum} y {segundoNum}) son iguales.\n\n\n");
+                }
+                else
+                {
+                    Console.WriteLine($"\n\n\nLos números introducidos ({primerNum} y {segundoNum}) son distintos.\n\n\n");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Introduce un valor numérico entero válido.");
+            }
+        }
+    }
+}
