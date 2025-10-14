0484ca3bb68b2da4c3331a7117b6f56de9da4e49
Manuel MR <manuel.monred@gmail.com>
2025-10-14 05:01:43 +0200

    Tema 3
    Tarea 7. Uso de SWITCH - CASE con ejemplo de tareas de casa. Uso también de un bucle DO - WHILE para repetir hasta salir de manera manual.

---
+++ Tema3/Tema3_Tarea07/Tema3_Tarea07/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3_Tarea07
+{
+    internal class Program
+    {
+        const string salida = "salir";
+
+        static void Main(string[] args)
+        {
+            string tareasHogar;
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("\n\nTema 3 - Tarea 7: Manuel MR\n\n");
+
+                Console.Write("Escribe qué tarea del hogar deseas hacer ahora mismo: ");
+                tareasHogar = Console.ReadLine();
+
+                switch (tareasHogar)
+                {
+                    case ("lavar los platos"):
+                        Console.WriteLine("\n\nLavando los platos...");
+                        break;
+                    case ("barrer el piso"):
+                        Console.WriteLine("\n\nBarriendo el piso...");
+                        break;
+                    case ("sacar la basura"):
+                        Console.WriteLine("\n\nSacando la basura...");
+                        break;
+                    case ("regar las plantas"):
+                        Console.WriteLine("\n\nRegando las plantas...");
+                        break;
+                    case ("hacer la cama"):
+                        Console.WriteLine("\n\nHaciendo la cama...");
+                        break;
+                    case ("salir"):
+                        break;
+                    default:
+                        Console.WriteLine("\n\nEscribe una tarea del hogar válida...");
+                        break;
+                }
+
+                if (tareasHogar != salida)
+                {
+                    Console.ReadKey();
+                }
+
+            } while (tareasHogar != salida);
+        }
+    }
+}
