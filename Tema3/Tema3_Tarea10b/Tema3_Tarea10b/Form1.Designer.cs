b064ef7e4c92f4acba56a75d98176b4fc85daac9
Manuel MR <manuel.monred@gmail.com>
2025-10-20 19:09:02 +0200

    Tema 3
    Segunda versión de la tarea 10.

---
+++ Tema3/Tema3_Tarea10b/Tema3_Tarea10b/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Tema3_Tarea10b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblValor = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.Wheat;
+            this.btnCalcular.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.btnCalcular.FlatAppearance.BorderSize = 2;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Font = new System.Drawing.Font("Lato", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(436, 227);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(240, 80);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.Wheat;
+            this.btnLimpiar.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.btnLimpiar.FlatAppearance.BorderSize = 2;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Font = new System.Drawing.Font("Lato", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(436, 382);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(240, 80);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Lato", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(67, 227);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(19, 29);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = " ";
+            // 
+            // lblValor
+            // 
+            this.lblValor.AutoSize = true;
+            this.lblValor.Font = new System.Drawing.Font("Lato", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValor.Location = new System.Drawing.Point(66, 91);
+            this.lblValor.Name = "lblValor";
+            this.lblValor.Size = new System.Drawing.Size(86, 34);
+            this.lblValor.TabIndex = 3;
+            this.lblValor.Text = "Valor:";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Font = new System.Drawing.Font("Lato Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValor.Location = new System.Drawing.Point(172, 88);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(308, 41);
+            this.txtValor.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(778, 744);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.lblValor);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Tarea 10 v2: Manuel MR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblValor;
+        private System.Windows.Forms.TextBox txtValor;
+    }
+}
+
---
+++ Tema3/Tema3_Tarea10b/Tema3_Tarea10b/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema3_Tarea10b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int dinero = int.Parse(txtValor.Text);
+
+            if (dinero >= 10000)
+            {
+                int resultado = dinero / 10000;
+                lblResultado.Text = $"{resultado} billetes de 10.000 pesetas.\n\n";
+                dinero -= resultado * 10000;
+            }
+
+            if (dinero >= 5000)
+            {
+                int resultado = dinero / 5000;
+                lblResultado.Text += $"{resultado} billetes de 5.000 pesetas.\n\n";
+                dinero -= resultado * 5000;
+            }
+
+            if (dinero >= 2000)
+            {
+                int resultado = dinero / 2000;
+                lblResultado.Text += $"{resultado} billetes de 2.000 pesetas.\n\n";
+                dinero -= resultado * 2000;
+            }
+
+            if (dinero >= 1000)
+            {
+                int resultado = dinero / 1000;
+                lblResultado.Text += $"{resultado} billetes de 1.000 pesetas.\n\n";
+                dinero -= resultado * 1000;
+            }
+
+            if (dinero >= 100)
+            {
+                int resultado = dinero / 100;
+                lblResultado.Text += $"{resultado} billetes de 100 pesetas.\n\n";
+                dinero -= resultado * 100;
+            }
+
+            if (dinero >= 25)
+            {
+                int resultado = dinero / 25;
+                lblResultado.Text += $"{resultado} billetes de 25 pesetas.\n\n\n\n";
+                dinero -= resultado * 25;
+            }
+
+            if (dinero != 0)
+            {
+                MessageBox.Show($"El dinero restante perdido es de {dinero} pesetas.", "Dinero perdido");
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = "";
+            txtValor.Clear();
+            txtValor.Focus();
+        }
+    }
+}
