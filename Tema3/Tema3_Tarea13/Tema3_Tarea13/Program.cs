4642061dfce6f0ac6a2e81ae23f852819e97af33
Manuel MR <manuel.monred@gmail.com>
2025-10-21 03:26:27 +0200

    Tema 3
    Tarea 13 ya resuelta, para practicar TRYPARSE.

---
+++ Tema3/Tema3_Tarea13/Tema3_Tarea13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3_Tarea13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n\nIntroduce un número entero: ");
+            string texto = Console.ReadLine();
+
+            bool exito = int.TryParse(texto, out int num);
+
+            Console.WriteLine($"\n\nÉxito: {exito}");
+            Console.WriteLine($"\nValor leído: {num}");
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
