16f7b4f09f8e1a13cf78f94f08fff48dc2bcaa8b
Manuel MR <manuel.monred@gmail.com>
2025-10-21 16:15:25 +0200

    Tema 3
    Uso de FOREACH con un ENUM en la tarea 14.

---
+++ Tema3/Tema3_Tarea14/Tema3_Tarea14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3_Tarea14
+{
+    internal class Program
+    {
+        enum TipoVehiculo
+        {
+            Coche,
+            Moto,
+            Bicicleta,
+            Autobús,
+            Camión
+        }
+
+        static void Main(string[] args)
+        {
+            string tipoVehiculo = "Tipos de vehículos disponibles:\n";
+
+            foreach (TipoVehiculo vehiculo in Enum.GetValues(typeof(TipoVehiculo)))
+            {
+                tipoVehiculo += $"\n- {vehiculo}";
+            }
+
+            Console.WriteLine($"{tipoVehiculo}");
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
