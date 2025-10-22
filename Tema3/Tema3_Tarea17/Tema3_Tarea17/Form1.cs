4cc4076e5f6cecb8172e8701add6ed231d51f30b
Manuel MR <manuel.monred@gmail.com>
2025-10-22 04:00:49 +0200

    Tema 3

---
+++ Tema3/Tema3_Tarea17/Tema3_Tarea17/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Tema3_Tarea17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            int numero;
+
+            numero = int.Parse(Interaction.InputBox("Introduce un número", "Número mayor y número menor"));
+
+            if (numero >= 0)
+            {
+                if 
+            }
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDoWhile_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
