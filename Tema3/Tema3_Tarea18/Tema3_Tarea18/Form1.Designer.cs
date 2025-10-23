a6e15902d1bf1cb133a16492d829a7c0fdc15d9e
Manuel MR <manuel.monred@gmail.com>
2025-10-23 03:15:43 +0200

    Tema 3
    Tarea 18.

---
+++ Tema3/Tema3_Tarea18/Tema3_Tarea18/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Tema3_Tarea18
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSuma = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSuma
+            // 
+            this.btnSuma.BackColor = System.Drawing.Color.Wheat;
+            this.btnSuma.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.btnSuma.FlatAppearance.BorderSize = 2;
+            this.btnSuma.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSuma.Font = new System.Drawing.Font("Cooper Black", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSuma.Location = new System.Drawing.Point(85, 153);
+            this.btnSuma.Name = "btnSuma";
+            this.btnSuma.Size = new System.Drawing.Size(600, 100);
+            this.btnSuma.TabIndex = 0;
+            this.btnSuma.Text = "Suma de números";
+            this.btnSuma.UseVisualStyleBackColor = false;
+            this.btnSuma.Click += new System.EventHandler(this.btnSuma_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(778, 444);
+            this.Controls.Add(this.btnSuma);
+            this.Name = "Form1";
+            this.Text = "Tarea 18: Manuel MR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSuma;
+    }
+}
+
---
+++ Tema3/Tema3_Tarea18/Tema3_Tarea18/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Tema3_Tarea18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            int numero;
+
+            numero = int.Parse(Interaction.InputBox("Introduce un número entero positivo", "Tarea 18", "numero"));
+        }
+    }
+}
