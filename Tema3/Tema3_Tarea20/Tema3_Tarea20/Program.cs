20c5547e7a4dd7a87b01dbabdedb5411b081a941
Manuel MR <manuel.monred@gmail.com>
2025-10-24 04:31:26 +0200

    Tema 3
    Tarea 20.

---
+++ Tema3/Tema3_Tarea20/Tema3_Tarea20/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3_Tarea20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 3 - Tarea 20: Manuel MR\n" +
+                              "----------------------------\n\n");
+
+            int suma = 0;
+            string texto = "Números pares: ";
+
+            for (int i = 1; i <= 50; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    if (i + 2 <= 50)
+                    {
+                        suma += i;
+                        texto += ($"{i}, ");
+                    }
+                    else
+                    {
+                        suma += i;
+                        texto += ($"{i}");
+                    }
+                }
+            }
+
+            Console.WriteLine($"{texto}" +
+                              $"\n\nLa suma de los números pares del 1 al 50 es: {suma}.");
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
