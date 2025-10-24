75a509b72930e8d5433e66d59eb5153cad276974
Manuel MR <manuel.monred@gmail.com>
2025-10-24 02:59:51 +0200

    Tema 3
    Tarea 19.

---
+++ Tema3/Tema3_Tarea19/Tema3_Tarea19/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3_Tarea19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 3 - Tarea 19: Manuel MR\n" +
+                              "----------------------------\n\n");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i +3 < 100)
+                    {
+                        Console.Write($"{i}, ");
+                    }
+                    else
+                    {
+                        Console.Write($"{i} ");
+                    }
+                }
+            }
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
