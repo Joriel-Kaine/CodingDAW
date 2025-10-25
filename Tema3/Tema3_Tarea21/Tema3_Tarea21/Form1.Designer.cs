4ec0c7a6c7b08dc2b2eda3cafae2e40aa8ccbc19
Manuel MR <manuel.monred@gmail.com>
2025-10-25 04:17:10 +0200

    Tema 3
    Tarea 21.

---
+++ Tema3/Tema3_Tarea21/Tema3_Tarea21/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Tema3_Tarea21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Lato", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(55, 163);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(158, 44);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Número:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Lato", 18F, System.Drawing.FontStyle.Bold);
+            this.txtNumero.Location = new System.Drawing.Point(235, 160);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(373, 51);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Wheat;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.button1.FlatAppearance.BorderSize = 2;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Lato", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(128, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(480, 100);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calcular Factorial";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(778, 744);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblNumero);
+            this.Name = "Form1";
+            this.Text = "Tarea 21: Manuel MR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tema3/Tema3_Tarea21/Tema3_Tarea21/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema3_Tarea21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool valor;
+            int factorial = 1;
+
+            if (valor = int.TryParse(txtNumero.Text, out int numero))
+            {
+                for (int i = numero; i > 0; i--)
+                {
+                    factorial *= i;
+                }
+
+                MessageBox.Show($"El factorial de {numero} es: {factorial}");
+            }
+            else
+            {
+                MessageBox.Show($"Debes introducir un número entero válido.");
+            }
+
+        }
+    }
+}
