504da70cdd22ab2cc886d1f37a023a4904919fd3
Manuel MR <manuel.monred@gmail.com>
2025-10-26 19:36:22 +0100

    Tema 3
    Tarea 24.

--- Tema3/Tema3_Tarea24/Tema3_Tarea24/Form1.cs
+++ Tema3/Tema3_Tarea24/Tema3_Tarea24/Form1.cs
@@ -21,10 +21,12 @@ namespace Tema3_Tarea24
         private void btnCalcularPesos_Click(object sender, EventArgs e)
         {
             double peso;
-            double primerRango = 0, segundoRango = 0,
+            int primerRango = 0, segundoRango = 0,
                 tercerRango = 0, cuartoRango = 0;
-            double porcentaje = 0;
-            double pesoMedio = 0;
+            int totalAlumnos;
+            int porcentajePrimerRango = 0, porcentajeSegundoRango = 0,
+                porcentajeTercerRango = 0, porcentajeCuartoRango = 0;
+            double pesoTotal = 0, pesoMedio = 0;
 
             peso = int.Parse(Interaction.InputBox("Introduce el peso del alumno", "Tarea 24", "peso"));
 
@@ -50,16 +52,28 @@ namespace Tema3_Tarea24
                     cuartoRango++;
                 }
 
-                pesoMedio += peso / 2;
+                pesoTotal += peso;
 
                 peso = int.Parse(Interaction.InputBox("Introduce el peso del alumno", "Tarea 24", "peso"));
             }
 
+            totalAlumnos = primerRango + segundoRango + tercerRango + cuartoRango;
+            porcentajePrimerRango = (primerRango * 100) / totalAlumnos;
+            porcentajeSegundoRango = (segundoRango * 100) / totalAlumnos;
+            porcentajeTercerRango = (tercerRango * 100) / totalAlumnos;
+            porcentajeCuartoRango = (cuartoRango * 100) / totalAlumnos;
+
+            pesoMedio = pesoTotal / totalAlumnos;
+
             MessageBox.Show($"Peso de alumnos con 50 kg o menos: {primerRango}\n" +
+                            $"con un porcentaje del total de alumnos de: {porcentajePrimerRango}%\n" +
                             $"Peso de alumnos entre 51 y 65 kg: {segundoRango}\n" +
+                            $"con un porcentaje del total de alumnos de: {porcentajeSegundoRango}%\n" +
                             $"Peso de alumnos entre 66 y 80 kg: {tercerRango}\n" +
-                            $"Peso de alumnos con más de 80 kg: {cuartoRango}\n\n\n" +
-                            $"El peso medio de todos los alumnos es: {pesoMedio:F2}");
+                            $"con un porcentaje del total de alumnos de: {porcentajeTercerRango}%\n" +
+                            $"Peso de alumnos con más de 80 kg: {cuartoRango}\n" +
+                            $"con un porcentaje del total de alumnos de: {porcentajeCuartoRango}%\n\n" +
+                            $"El peso medio de todos los alumnos es: {pesoMedio:F2} kg", "Calcular Pesos");
         }
     }
 }
