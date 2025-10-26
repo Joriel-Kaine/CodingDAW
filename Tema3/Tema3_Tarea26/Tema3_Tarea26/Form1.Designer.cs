309545605ce9328c492683b769a289b634899320
Manuel MR <manuel.monred@gmail.com>
2025-10-26 21:18:11 +0100

    Tema 3
    Tarea 26.

---
+++ Tema3/Tema3_Tarea26/Tema3_Tarea26/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Tema3_Tarea26
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrarTabla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMostrarTabla
+            // 
+            this.btnMostrarTabla.BackColor = System.Drawing.Color.Wheat;
+            this.btnMostrarTabla.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.btnMostrarTabla.FlatAppearance.BorderSize = 2;
+            this.btnMostrarTabla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMostrarTabla.Font = new System.Drawing.Font("Lato", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrarTabla.Location = new System.Drawing.Point(199, 114);
+            this.btnMostrarTabla.Name = "btnMostrarTabla";
+            this.btnMostrarTabla.Size = new System.Drawing.Size(380, 100);
+            this.btnMostrarTabla.TabIndex = 0;
+            this.btnMostrarTabla.Text = "Mostrar Tablas";
+            this.btnMostrarTabla.UseVisualStyleBackColor = false;
+            this.btnMostrarTabla.Click += new System.EventHandler(this.btnMostrarTabla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(778, 344);
+            this.Controls.Add(this.btnMostrarTabla);
+            this.Name = "Form1";
+            this.Text = "Tema 3 - Tarea 26: Manuel MR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrarTabla;
+    }
+}
+
---
+++ Tema3/Tema3_Tarea26/Tema3_Tarea26/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema3_Tarea26
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrarTabla_Click(object sender, EventArgs e)
+        {
+            int resultado = 0;
+            string tabla;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                tabla = "";
+
+                for (int j = 1; j <= 10; j++)
+                {
+                    resultado = i * j;
+                    tabla += $"{i} * {j} = {resultado}\n";
+                }
+
+                MessageBox.Show(tabla, $"Tabla del {i}");
+            }
+        }
+    }
+}
