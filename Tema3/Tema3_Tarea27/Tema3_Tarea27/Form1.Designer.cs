25a2079b91410e8458f8b49a0e9aa482296f6d7e
Manuel MR <manuel.monred@gmail.com>
2025-10-26 23:03:42 +0100

    Tema 3
    Tarea 27.

---
+++ Tema3/Tema3_Tarea27/Tema3_Tarea27/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Tema3_Tarea27
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.BackColor = System.Drawing.Color.Wheat;
+            this.btnMostrar.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.btnMostrar.FlatAppearance.BorderSize = 2;
+            this.btnMostrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMostrar.Font = new System.Drawing.Font("Lato", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrar.Location = new System.Drawing.Point(193, 119);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(380, 100);
+            this.btnMostrar.TabIndex = 0;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = false;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(778, 344);
+            this.Controls.Add(this.btnMostrar);
+            this.Name = "Form1";
+            this.Text = "Tema 3 - Tarea 27: Manuel MR";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrar;
+    }
+}
+
---
+++ Tema3/Tema3_Tarea27/Tema3_Tarea27/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema3_Tarea27
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            string resultado = "";
+            int suma = 0;
+
+            for (int i = 3; i <= 100; i+=3)
+            {
+                if (i + 3 <= 100)
+                {
+                    if (i % 7 == 0)
+                    {
+                        resultado += $"{i},\n";
+                        suma += i;
+                    }
+                    else
+                    {
+                        resultado += $"{i}, ";
+                        suma += i;
+                    }
+                }
+                else
+                {
+                    resultado += $"{i}";
+                    suma += i;
+                }
+            }
+
+            MessageBox.Show($"Serie:\n" +
+                            $"{resultado}\n\n" +
+                            $"La suma es: {suma}", "Suma y visualización de serie aritmética");
+        }
+    }
+}
