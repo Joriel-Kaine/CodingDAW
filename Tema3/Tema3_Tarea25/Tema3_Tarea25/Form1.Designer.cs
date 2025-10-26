706dfb3cf9313967edd46576f4f300bde04c12bf
Manuel MR <manuel.monred@gmail.com>
2025-10-26 20:51:05 +0100

    Tema 3
    Tarea 25.

---
+++ Tema3/Tema3_Tarea25/Tema3_Tarea25/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Tema3_Tarea25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnMostrarTabla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Lato", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(125, 283);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(124, 34);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Número:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Lato Semibold", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(272, 280);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(307, 41);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // btnMostrarTabla
+            // 
+            this.btnMostrarTabla.BackColor = System.Drawing.Color.Wheat;
+            this.btnMostrarTabla.FlatAppearance.BorderColor = System.Drawing.Color.SlateGray;
+            this.btnMostrarTabla.FlatAppearance.BorderSize = 2;
+            this.btnMostrarTabla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMostrarTabla.Font = new System.Drawing.Font("Lato", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMostrarTabla.Location = new System.Drawing.Point(259, 466);
+            this.btnMostrarTabla.Name = "btnMostrarTabla";
+            this.btnMostrarTabla.Size = new System.Drawing.Size(320, 100);
+            this.btnMostrarTabla.TabIndex = 2;
+            this.btnMostrarTabla.Text = "Mostrar Tabla";
+            this.btnMostrarTabla.UseVisualStyleBackColor = false;
+            this.btnMostrarTabla.Click += new System.EventHandler(this.btnMostrarTabla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(778, 744);
+            this.Controls.Add(this.btnMostrarTabla);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblNumero);
+            this.Name = "Form1";
+            this.Text = "Tema 3 - Tarea 25: Manuel MR";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnMostrarTabla;
+    }
+}
+
---
+++ Tema3/Tema3_Tarea25/Tema3_Tarea25/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tema3_Tarea25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMostrarTabla_Click(object sender, EventArgs e)
+        {
+            bool entrada;
+            int resultado = 0;
+            string tabla = "";
+
+            entrada = int.TryParse(txtNumero.Text, out int numero);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                resultado = numero * i;
+                tabla += $"{numero} * {i} = {resultado}\n";
+            }
+
+            MessageBox.Show($"Tabla del {numero}\n\n" +
+                            $"{tabla}", "Tabla de multiplicar");
+        }
+    }
+}
