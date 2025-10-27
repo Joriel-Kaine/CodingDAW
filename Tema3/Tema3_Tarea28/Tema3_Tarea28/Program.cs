83face8cf4b0ffef43b2e54d8d842f3cff90dea4
Manuel MR <manuel.monred@gmail.com>
2025-10-27 03:56:53 +0100

    Tema 3
    Tarea 28.

---
+++ Tema3/Tema3_Tarea28/Tema3_Tarea28/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3_Tarea28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 3 - Tarea 28: Manuel MR\n" +
+                              "----------------------------");
+
+            bool entrada;
+            bool primo = true;
+
+            Console.Write("\n\nIntroduce un número entero: ");
+            entrada = int.TryParse(Console.ReadLine(), out int numero);
+
+            if (entrada)
+            {
+                for (int i = 2; i < numero; i++)
+                {
+                    if (numero % i == 0)
+                    {
+                        primo = false;
+                    }
+                }
+
+                if (primo)
+                {
+                    Console.WriteLine($"\n\nEl número {numero} es un número pri1mo.");
+                }
+                else
+                {
+                    Console.WriteLine($"\n\nEl número {numero} no es un número primo.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n\nIntroduce un número entero válido.");
+            }
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tema3/Tema3_Tarea28/Tema3_Tarea28/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Tema3_Tarea28")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tema3_Tarea28")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("013d3f37-bc23-4cea-b34e-d11ea87ae677")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
