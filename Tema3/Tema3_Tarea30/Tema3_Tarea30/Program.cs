10a52fed5fa42a2534ba3c282de930092ecc710c
Manuel MR <manuel.monred@gmail.com>
2025-10-28 21:15:42 +0100

    Tema 3

---
+++ Tema3/Tema3_Tarea30/Tema3_Tarea30/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema3_Tarea30
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 3 - Tarea 30: Manuel MR\n" +
+                              "----------------------------\n\n");
+
+            bool entradaNum;
+            double serie = 0;
+
+            Console.Write("Introduce un número entero positivo: ");
+            entradaNum = int.TryParse(Console.ReadLine(), out int numero);
+
+            if (numero > 0)
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        serie -= 1.0 / (double)i;
+                    }
+                    else
+                    {
+                        serie += 1.0 / (double)i;
+                    }
+                }
+
+                Console.WriteLine($"\n\nEl resultado de la serie es: {serie}");
+            }
+            else
+            {
+                Console.WriteLine("\nDebes introducir un número válido.");
+            }
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
