157c110423e16123e3aae0f92ba2d49c5796d368
Manuel MR <manuel.monred@gmail.com>
2025-11-04 18:28:03 +0100

    Tema 4

---
+++ Tema4/Tema4_Tarea02/Tema4_Tarea02/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Tema4_Tarea02
+{
+    internal class Program
+    {
+        static bool EsDivisible (int num1, int num2)
+        {
+            if (num1 % num2 == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 4 - Tarea 2: Manuel MR ®\n" +
+                              "═════════════════════════════\n\n");
+
+            bool esCorrecto, resultado;
+            int num1, num2;
+
+            try
+            {
+                Console.Write("Introduce el primer número: ");
+                esCorrecto = int.TryParse(Console.ReadLine(), out num1);
+
+                Console.Write("\nIntroduce el segundo número: ");
+                esCorrecto = int.TryParse(Console.ReadLine(), out num2);
+                if (esCorrecto)
+                {
+                    resultado = EsDivisible(num1, num2);
+
+                    if (resultado)
+                    {
+                        Console.WriteLine($"\n{num1} es divisible por {num2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"\n{num1} no es divisible por {num2}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nIntroduce un valor númerico válido.");
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("\n\nNo se puede dividir por 0.");
+            }
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
