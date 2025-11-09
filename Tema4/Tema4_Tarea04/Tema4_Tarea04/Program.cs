4f4c3b756f174e3d00688688089f469761e1f068
Manuel MR <manuel.monred@gmail.com>
2025-11-09 23:46:57 +0100

    Tema 4

---
+++ Tema4/Tema4_Tarea04/Tema4_Tarea04/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Tema4_Tarea04
+{
+    internal class Program
+    {
+        static int ValorAbsoluto (int num)
+        {
+            int absoluto = (num < 0) ? -num : num;
+
+            return absoluto;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 4 - Tarea 4: Manuel MR ®\n" +
+                              "═════════════════════════════\n\n");
+
+            bool esCorrecto;
+            int num, absoluto;
+
+            Console.Write("Introduce un número: ");
+            esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+            if (esCorrecto)
+            {
+                absoluto = ValorAbsoluto(num);
+                Console.WriteLine($"\nEl valor absoluto de {num} es: {absoluto}");
+            }
+            else
+            {
+                Console.WriteLine("Introduce un valor numérico.");
+            }
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
