96410027eccd8a1683280a0ce4b3f2d2f775b7ff
Manuel MR <manuel.monred@gmail.com>
2025-11-10 02:09:24 +0100

    Tema 4

---
+++ Tema4/Tema4_Tarea06/Tema4_Tarea06/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Tema4_Tarea06
+{
+    internal class Program
+    {
+        static void Intercambiar (ref int num1, ref int num2)
+        {
+            int intercambio;
+
+            intercambio = num1;
+            num1 = num2;
+            num2 = intercambio;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 4 - Tarea 6: Manuel MR ®\n" +
+                              "═════════════════════════════\n\n");
+
+            bool esCorrecto1, esCorrecto2;
+            int num1, num2;
+
+            Console.Write("Introduce el primer número: ");
+            esCorrecto1 = int.TryParse(Console.ReadLine(), out num1);
+
+            Console.Write("\nIntroduce el segundo número: ");
+            esCorrecto2 = int.TryParse(Console.ReadLine(), out num2);
+
+            if (esCorrecto1 && esCorrecto2)
+            {
+                Intercambiar(ref num1, ref num2);
+                Console.WriteLine($"\n═> El resultado de los números introducidos intercambiados es: {num1} <-> {num2}");
+            }
+            else
+            {
+                Console.WriteLine("\n\nIntroduce un valor numérico.");
+            }
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
