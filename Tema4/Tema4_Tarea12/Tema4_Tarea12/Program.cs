eaa13f2a240281fc38d23aec59161071ab708c39
Manuel MR <manuel.monred@gmail.com>
2025-11-16 22:21:10 +0100

    Tema 4

---
+++ Tema4/Tema4_Tarea12/Tema4_Tarea12/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Tema4_Tarea12
+{
+    internal class Program
+    {
+        static int LeerNumero (string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("Introduce un valor numérico válido.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+
+        static int CalcularPotencia (int bbase, int exponente)
+        {
+            int potencia = 1;
+
+            checked
+            {
+                for (int i = 0; i < exponente; i++)
+                {
+                    potencia *= bbase;
+                }
+            }
+
+            return potencia;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 4 - Tarea 12: Manuel MR ®\n" +
+                              "══════════════════════════════\n\n");
+
+            int bbase, exponente;
+
+            try
+            {
+                bbase = LeerNumero("\nIntroduce la base: ");
+                exponente = LeerNumero("\nIntroduce el exponente: ");
+
+                Console.WriteLine($"\n═> La potencia es: {CalcularPotencia(bbase, exponente)}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("\n\nError de OverFlow en la operación.");
+            }
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tema4/Tema4_Tarea13/Tema4_Tarea13/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Tema4_Tarea13
+{
+    internal class Program
+    {
+        static int LeerNumero (string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\n\nIntroduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+
+        static int CalcularPotencia (int bbase, int exponente)
+        {
+            int potencia = 1;
+
+            checked
+            {
+                for (int i = 0; i < exponente; i++)
+                {
+                    potencia *= bbase;
+                }
+            }
+
+            return potencia;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 4 - Tarea 13: Manuel MR ®\n" +
+                              "══════════════════════════════\n\n");
+
+            int bbase;
+
+            try
+            {
+                bbase = LeerNumero("\nIntroduce la base: ");
+
+                Console.WriteLine($"\n═> La potencia con exponente 2 es: {CalcularPotencia(bbase, exponente: 2)}" +
+                                  $"\n═> La potencia con exponente 5 es: {CalcularPotencia(bbase, exponente: 5)}" +
+                                  $"\n═> La potencia con exponente 7 es: {CalcularPotencia(bbase, exponente: 7)}");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("\n\nError de OverFlow en la operación.");
+            }
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
