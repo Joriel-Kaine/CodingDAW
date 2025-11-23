82633aab25d9b23e11f00895ae9a17c129d452a0
Manuel MR <manuel.monred@gmail.com>
2025-11-23 04:27:41 +0100

    Tema 4

---
+++ Tema4/Tema4_Tarea19/Tema4_Tarea19/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Tema4_Tarea19
+{
+    internal class Program
+    {
+        static int LeerNumero (string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\n\n Introduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+
+        static int SepararPesetas (int diezMil, int cincoMil, int dosMil, int mil, int veintiCinco, int uno)
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 4 - Tarea 19: Manuel MR ®\n" +
+                              "══════════════════════════════\n\n");
+
+
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
