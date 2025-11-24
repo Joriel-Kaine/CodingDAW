acdd68c40761f3b53962e0c8fbefaacebe3d87f1
Manuel MR <manuel.monred@gmail.com>
2025-11-24 02:08:17 +0100

    Tema 4

---
+++ Tema4/Tema4_Tarea22/Tema4_Tarea22/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Tema4_Tarea22
+{
+    internal class Program
+    {
+        static int LeerNumero (string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\n\nIntroduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+
+        static int CalcularDivisionEntera (int num1, int num2)
+        {
+            int divisionEntera = 0;
+
+            while (num1 >= num2)
+            {
+                num1 -= num2;
+                divisionEntera++;
+            }
+
+            return divisionEntera;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 4 - Tarea 22: Manuel MR ®\n" +
+                              "══════════════════════════════\n\n");
+
+            int num1, num2;
+
+            num1 = LeerNumero("\nIntroduce el primer número: ");
+            num2 = LeerNumero("\nIntroduce el segundo número: ");
+
+            Console.WriteLine($"\n═> El resultado de la división entera de {num1}/{num2} es: " +
+                              $"{CalcularDivisionEntera(num1, num2)}");
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+        }
+    }
+}
