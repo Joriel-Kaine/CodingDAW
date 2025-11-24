7ace12bb1c2e2ad9c1424a7bd08224d845620d9a
Manuel MR <manuel.monred@gmail.com>
2025-11-24 02:21:31 +0100

    Tema 4

---
+++ Tema4/Tema4_Tarea23/Tema4_Tarea23/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Tema4_Tarea23
+{
+    internal class Program
+    {
+        static int LeerNumero(string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\n\nIntroduce valores numéricos válidos");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+
+        static int ContarDigitos(int num)
+        {
+            int contador = 0;
+
+            while (num > 0)
+            {
+                num /= 10;
+                contador++;
+            }
+
+            return contador;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 4 - Tarea 23: Manuel MR ®\n" +
+                              "══════════════════════════════\n\n");
+
+            int num;
+
+            num = LeerNumero("\nIntroduce un número entero: ");
+
+            Console.WriteLine($"\n═> Dígitos contados del número {num}: {ContarDigitos(num)}");
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+        }
+    }
+}
