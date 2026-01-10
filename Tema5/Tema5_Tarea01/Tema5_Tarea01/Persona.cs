e16628b9c467fa9c6b9c4e75c60e592cb83311b9
Manuel MR <manuel.monred@gmail.com>
2026-01-10 05:29:46 +0100

    Tema 5

--- Tema5/Tema5_Tarea01/Tema5_Tarea01/Persona.cs
+++ Tema5/Tema5_Tarea01/Tema5_Tarea01/Persona.cs
@@ -8,6 +8,50 @@ namespace Tema5_Tarea01
 {
     public class Persona
     {
+        private string _nombre;
+        private int _edad;
+        private string _telefono;
+        private string _sexo;
+        private bool _casado;
 
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public int Edad
+        {
+            get { return _edad; }
+            set 
+            {
+                if (_edad >= 0 || _edad <= 100)
+                {
+                    _edad = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("\nLa edad debe estar entre 0 y 100");
+                }
+            }
+        }
+
+        public string Telefono
+        {
+            get { return _telefono; }
+            set { _telefono = value; }
+        }
+
+        public string Sexo
+        {
+            get { return _sexo; }
+            set { _sexo = value; }
+        }
+
+        public bool Casado
+        {
+            get { return _casado; }
+            set { _casado = value; }
+        }
     }
 }
