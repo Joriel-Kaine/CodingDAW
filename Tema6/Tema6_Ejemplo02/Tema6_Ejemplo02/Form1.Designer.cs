a6798c31b676313a5a69704f435e0fbdc11ce848
Manuel MR <manuel.monred@gmail.com>
2026-01-11 04:34:04 +0100

    Tema 6

---
+++ Tema6/Tema6_Ejemplo02/Tema6_Ejemplo02/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Tema6_Ejemplo02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            btnLeer = new Button();
+            btnMostrar = new Button();
+            btnDividirPos = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Lato", 19F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(127, 57);
+            label1.Name = "label1";
+            label1.Size = new Size(525, 46);
+            label1.TabIndex = 0;
+            label1.Text = "Vector de 10 números enteros";
+            // 
+            // btnLeer
+            // 
+            btnLeer.BackColor = Color.Wheat;
+            btnLeer.FlatAppearance.BorderColor = Color.SteelBlue;
+            btnLeer.FlatAppearance.BorderSize = 2;
+            btnLeer.FlatStyle = FlatStyle.Flat;
+            btnLeer.Font = new Font("Lato", 17F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLeer.Location = new Point(111, 217);
+            btnLeer.Name = "btnLeer";
+            btnLeer.Size = new Size(280, 80);
+            btnLeer.TabIndex = 1;
+            btnLeer.Text = "Leer Vector";
+            btnLeer.UseVisualStyleBackColor = false;
+            btnLeer.Click += btnLeer_Click;
+            // 
+            // btnMostrar
+            // 
+            btnMostrar.BackColor = Color.Wheat;
+            btnMostrar.FlatAppearance.BorderColor = Color.SteelBlue;
+            btnMostrar.FlatAppearance.BorderSize = 2;
+            btnMostrar.FlatStyle = FlatStyle.Flat;
+            btnMostrar.Font = new Font("Lato", 17F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnMostrar.Location = new Point(397, 217);
+            btnMostrar.Name = "btnMostrar";
+            btnMostrar.Size = new Size(280, 80);
+            btnMostrar.TabIndex = 2;
+            btnMostrar.Text = "Mostrar Vector";
+            btnMostrar.UseVisualStyleBackColor = false;
+            btnMostrar.Click += btnMostrar_Click;
+            // 
+            // btnDividirPos
+            // 
+            btnDividirPos.BackColor = Color.Wheat;
+            btnDividirPos.FlatAppearance.BorderColor = Color.SteelBlue;
+            btnDividirPos.FlatAppearance.BorderSize = 2;
+            btnDividirPos.FlatStyle = FlatStyle.Flat;
+            btnDividirPos.Font = new Font("Lato", 17F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDividirPos.Location = new Point(254, 303);
+            btnDividirPos.Name = "btnDividirPos";
+            btnDividirPos.Size = new Size(280, 80);
+            btnDividirPos.TabIndex = 3;
+            btnDividirPos.Text = "Dividir Posición";
+            btnDividirPos.UseVisualStyleBackColor = false;
+            btnDividirPos.Click += btnDividirPos_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightGray;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnDividirPos);
+            Controls.Add(btnMostrar);
+            Controls.Add(btnLeer);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Tema 6 - Ejemplo 2: Manuel MR ®";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnLeer;
+        private Button btnMostrar;
+        private Button btnDividirPos;
+    }
+}
--- Tema6/Tema6_Ejemplo02/Tema6_Ejemplo02/Form1.cs
+++ Tema6/Tema6_Ejemplo02/Tema6_Ejemplo02/Form1.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualBasic;
 
-namespace Tema6_Ejemplo01
+namespace Tema6_Ejemplo02
 {
     public partial class Form1 : Form
     {
@@ -8,6 +8,11 @@ namespace Tema6_Ejemplo01
         {
             InitializeComponent();
         }
+
+        const int Tamano = 10;
+
+        // Declaramos y damos tamaño al vector.
+        int[] vector = new int[Tamano];
 
         int LeerNumero(string mensaje)
         {
@@ -30,15 +35,11 @@ namespace Tema6_Ejemplo01
             return num;
         }
 
-        const int Tamano = 5;
-
-        // Declaramos y damos tamaño al vector.
-        int[] vector = new int[Tamano];
-
         void LeerVector(int[] vector)
         {
             string mensaje = "";
 
+            // Leemos todos los elementos del vector.
             for (int i = 0; i < vector.Length; i++)
             {
                 vector[i] = LeerNumero(mensaje) + i;
@@ -48,8 +49,8 @@ namespace Tema6_Ejemplo01
         string MostrarVector(int[] vector)
         {
             string texto;
-            texto = "Elementos del vector: \n";
 
+            texto = "Elementos del vector: \n";
             for (int i = 0; i < vector.Length; i++)
             {
                 texto += $"{vector[i]}, ";
@@ -58,9 +59,21 @@ namespace Tema6_Ejemplo01
             return texto;
         }
 
+        int SumarVector(int[] vector)
+        {
+            int suma = 0;
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                suma += vector[i];
+            }
+
+            return suma;
+        }
+
         private void btnLeer_Click(object sender, EventArgs e)
         {
-            LeerVector(vector); 
+            LeerVector(vector);
         }
 
         private void btnMostrar_Click(object sender, EventArgs e)
@@ -69,7 +82,24 @@ namespace Tema6_Ejemplo01
 
             texto = MostrarVector(vector);
 
-            MessageBox.Show(texto, "Mostrar Vector");
+            MessageBox.Show(texto);
+        }
+
+        private void btnDividirPos_Click(object sender, EventArgs e)
+        {
+            int posicion, suma;
+
+            posicion = int.Parse(Interaction.InputBox("Introduce la posición a dividir."));
+
+            if (posicion >= 0 && posicion < vector.Length)
+            {
+                suma = SumarVector(vector);
+                MessageBox.Show($"Resultado: {suma / vector[posicion]}");
+            }
+            else
+            {
+                MessageBox.Show("La posición sale de los límites del vector.");
+            }
         }
     }
 }
