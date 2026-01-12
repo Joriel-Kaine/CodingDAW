725651ede2c4fd74c7eec3379e07e07df08b5a65
Manuel MR <manuel.monred@gmail.com>
2026-01-12 03:27:00 +0100

    Tema 6 - Vectores de strings y ordenación

---
+++ Tema6/Tema6_Ejemplo05/Tema6_Ejemplo05/FuncionesAlumnos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6_Ejemplo05
+{
+    public class FuncionesAlumnos
+    {
+        private static int LeerNumero(string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\nIntroduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+
+        public static void LeerAlumnosYNotas(string[] vectorAlumnos, int[] vectorNotas)
+        {
+            // Leemos todos los elementos del vector.
+            for (int i = 0; i < vectorAlumnos.Length; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ Tema6/Tema6_Ejemplo05/Tema6_Ejemplo05/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tema6_Ejemplo05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 6 - Ejemplo 5: Manuel MR ®\n" +
+                              "═══════════════════════════════\n\n");
+
+
+
+            Console.WriteLine("\n\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
