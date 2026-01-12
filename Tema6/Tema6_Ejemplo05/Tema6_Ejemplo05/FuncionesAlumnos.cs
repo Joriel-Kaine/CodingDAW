e8b5161a5d86b10a0852074c1234bab5ec095800
Manuel MR <manuel.monred@gmail.com>
2026-01-12 17:05:08 +0100

    Tema 6 - Vectores de strings y ordenación

--- Tema6/Tema6_Ejemplo05/Tema6_Ejemplo05/FuncionesAlumnos.cs
+++ Tema6/Tema6_Ejemplo05/Tema6_Ejemplo05/FuncionesAlumnos.cs
@@ -33,8 +33,133 @@ namespace Tema6_Ejemplo05
             // Leemos todos los elementos del vector.
             for (int i = 0; i < vectorAlumnos.Length; i++)
             {
+                Console.Write($"Introduce el nombre del alumno {i}: ");
+                vectorAlumnos[i] = Console.ReadLine();
 
+                do
+                {
+                    vectorNotas[i] = LeerNumero($"Introduce la nota de {vectorAlumnos[i]} (entre 0 y 10): ");
+
+                } while (vectorNotas[i] < 0 || vectorNotas[i] > 10);
             }
+        }
+
+        // Mostramos el listado con todos los alumnos y sus notas.
+        public static string MostrarAlumnosYNotas(string[] vectorAlumnos, int[] vectorNotas)
+        {
+            string texto = "Listado: \n";
+
+            for (int i = 0; i < vectorAlumnos.Length; i++)
+            {
+                texto += $"{vectorAlumnos[i]}: {vectorNotas[i]} \n";
+            }
+
+            return texto;
+        }
+
+        // Método para intercambiar dos notas.
+        private static void IntercambiarNotas(ref int nota1, ref int nota2)
+        {
+            int aux;
+
+            aux = nota1;
+            nota1 = nota2;
+            nota2 = aux;
+        }
+
+        // Método para intercambiar dos nombres.
+        private static void IntercambiarNombres(ref string nombre1, ref string nombre2)
+        {
+            string aux;
+
+            aux = nombre1;
+            nombre1 = nombre2;
+            nombre2 = aux;
+        }
+
+        // Método que devuelve la posición de un alumno.
+        // Devuelve -1 si no lo encuentra.
+        public static int BuscarAlumno(string[] vectorAlumnos, string nombre)
+        {
+            int resultado, contador = 0;
+            bool esEncontrado = false;
+
+            while (!esEncontrado && contador < vectorAlumnos.Length)
+            {
+                if (vectorAlumnos[contador] == nombre)
+                {
+                    esEncontrado = true;
+                }
+                else
+                {
+                    contador++;
+                }
+            }
+
+            if (esEncontrado)
+            {
+                resultado = contador;
+            }
+            else
+            {
+                resultado = -1;
+            }
+
+            return resultado;
+        }
+
+        // Algoritmo de ordenación.
+        public static void OrdenarVectoresPorNotas(string[] vectorAlumnos, int[] vectorNotas)
+        {
+            int tamano = vectorAlumnos.Length;
+
+            for (int i = 0; i < tamano - 1; i++)
+            {
+                for (int j = i + 1; j < tamano; j++)
+                {
+                    IntercambiarNotas(ref vectorNotas[i], ref vectorNotas[j]);
+                    // También intercambiamos los nombres para que estén en la misma posición.
+                    IntercambiarNombres(ref vectorAlumnos[i], ref vectorAlumnos[j]);
+                }
+            }
+        }
+
+        public static void OrdenarVectoresPorNombres(string[] vectorAlumnos, int[] vectorNotas)
+        {
+            int tamano = vectorAlumnos.Length;
+
+            for (int i = 0; i < tamano - 1; i++)
+            {
+                for (int j = i + 1; j < tamano; j++)
+                {
+                    // Si es menor que 0 significa que la cadena j es menor alfabéticamente que la cadena i.
+                    if (string.Compare(vectorAlumnos[j], vectorAlumnos[i]) < 0)
+                    {
+                        IntercambiarNombres(ref vectorAlumnos[i], ref vectorAlumnos[j]);
+                        // También intercambiamos las notas para que estén en la misma posición.
+                        IntercambiarNotas(ref vectorNotas[i], ref vectorNotas[j]);
+                    }
+                }
+            }
+        }
+
+        public static int Menu()
+        {
+            int opcion;
+
+            Console.WriteLine("\n   MENÚ\n" +
+                              "\n1. Introducir todos los datos." +
+                              "\n2. Mostrar todos los datos." +
+                              "\n3. Buscar alumno." +
+                              "\n4. Ordenar por notas." +
+                              "\n5. Ordenar alfabéticamente." +
+                              "\n6. Salir del programa.");
+
+            opcion = LeerNumero("\nIntroduce una opción del menú: ");
+
+            Console.WriteLine(); // Línea en blanco.
+
+            return opcion;
         }
     }
 }
--- Tema6/Tema6_Ejemplo05/Tema6_Ejemplo05/Program.cs
+++ Tema6/Tema6_Ejemplo05/Tema6_Ejemplo05/Program.cs
@@ -7,7 +7,50 @@
             Console.WriteLine("\nTema 6 - Ejemplo 5: Manuel MR ®\n" +
                               "═══════════════════════════════\n\n");
 
+            // Definimos y creamos los vectores
+            const int Tamano = 4;
+            string[] vectorAlumnos = new string[Tamano];
+            int[] vectorNotas = new int[Tamano];
 
+            int opcion;
+
+            do
+            {
+                opcion = FuncionesAlumnos.Menu();
+
+                switch (opcion)
+                {
+                    case 1:
+                        FuncionesAlumnos.LeerAlumnosYNotas(vectorAlumnos, vectorNotas);
+                        break;
+                    case 2:
+                        FuncionesAlumnos.MostrarAlumnosYNotas(vectorAlumnos, vectorNotas);
+                        break;
+                    case 3:
+                        Console.Write("\nIntroduce el nombre del alumno a buscar: ");
+                        string nombre = Console.ReadLine();
+                        int posicion = FuncionesAlumnos.BuscarAlumno(vectorAlumnos, nombre);
+
+                        if (posicion >= 0)
+                        {
+                            Console.WriteLine($"\nAlumno: {vectorAlumnos[posicion]} Nota: {vectorNotas[posicion]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nNo se ha encontrado el alumno.");
+                        }
+                        break;
+                    case 4:
+                        FuncionesAlumnos.OrdenarVectoresPorNotas(vectorAlumnos, vectorNotas);
+                        break;
+                    case 5:
+                        FuncionesAlumnos.OrdenarVectoresPorNombres(vectorAlumnos, vectorNotas);
+                        break;
+                }
+
+                Console.WriteLine("\nFin del programa.");
+
+            } while (opcion != 6);
 
             Console.WriteLine("\n\nPulsa una tecla para salir...");
             Console.ReadKey();
