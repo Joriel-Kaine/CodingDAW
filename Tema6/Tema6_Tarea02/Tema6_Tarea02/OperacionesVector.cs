aab4be753013694e71cafe4c4cc764aeef851ce1
Manuel MR <manuel.monred@gmail.com>
2026-01-15 18:18:54 +0100

    Tarea 2 - Media de vector con función - CONSOLA

---
+++ Tema6/Tema6_Tarea02/Tema6_Tarea02/OperacionesVector.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6_Tarea02
+{
+    public class OperacionesVector
+    {
+        public static int LeerNumero(string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\nIntroduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+
+        public static void LeerVector(int[] vector)
+        {
+            int tamano = vector.Length;
+
+            for (int i = 0; i < tamano; i++)
+            {
+                vector[i] = LeerNumero($"Introduce un valor ({i + 1} de {tamano})");
+            }
+        }
+
+        public static 
+
+        public static int SumarVector(int[] vector)
+        {
+            int suma = 0;
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                suma += vector[i];
+            }
+
+            return suma;
+        }
+
+        public static double MostrarMediaVector(string[] vector)
+        {
+
+        }
+
+        public static int Menu()
+        {
+
+        }
+    }
+}
---
+++ Tema6/Tema6_Tarea02/Tema6_Tarea02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tema6_Tarea02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 6 - Tarea 2: Manuel MR ®\n" +
+                              "═════════════════════════════\n\n");
+
+
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "Pulsa una tecla para salir...");
+        }
+    }
+}
