58c396bf73a8bbcd4be3cf704a52dcd6af24fd06
Manuel MR <manuel.monred@gmail.com>
2026-01-16 13:56:23 +0100

    Tarea 3 - Menor valor vector - CONSOLA

---
+++ Tema6/Tema6_Tarea03/Tema6_Tarea03/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Tema6_Tarea03
+{
+    internal class Program
+    {
+        const int Tamano = 10;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 6 - Tarea 3: Manuel MR ®\n" +
+                              "═════════════════════════════\n\n");
+
+            int[] numeros = new int[Tamano];
+            int opcion;
+
+
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
