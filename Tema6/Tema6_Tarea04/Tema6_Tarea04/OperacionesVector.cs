3456fa167734f1cec4d43b01ec8089927e87b528
Manuel MR <manuel.monred@gmail.com>
2026-01-16 18:13:14 +0100

    Tarea 4 - Vector al revés - CONSOLA

---
+++ Tema6/Tema6_Tarea04/Tema6_Tarea04/OperacionesVector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6_Tarea04
+{
+    public class OperacionesVector
+    {
+        public static int LeerNumero()
+        {
+
+        }
+
+        public static void LeerVector()
+        {
+
+        }
+
+        public static string GenerarTextoVector()
+        {
+
+        }
+
+        public static int GenerarVectorInvertido()
+        {
+
+        }
+
+        public static int Menu()
+        {
+
+        }
+    }
+}
---
+++ Tema6/Tema6_Tarea04/Tema6_Tarea04/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Tema6_Tarea04
+{
+    internal class Program
+    {
+        const int Tamano = 10;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 6 - Tarea 4: Manuel MR ®\n" +
+                              "═════════════════════════════\n\n");
+
+            int[] numeros = new int[Tamano];
+            int opcion;
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
