564c6fd03392554d60def0fa5babdc3d07f5a8f7
Manuel MR <manuel.monred@gmail.com>
2026-01-17 17:33:28 +0100

    Tarea 5 - Buscar número y contar repeticiones - CONSOLA

---
+++ Tema6/Tema6_Tarea05/Tema6_Tarea05/OperacionesVector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6_Tarea05
+{
+    public class OperacionesVector
+    {
+        public static int LeerNumero()
+        {
+            return 0;
+        }
+
+        public static void LeerVector()
+        {
+
+        }
+
+        public static string GenerarTextoVector()
+        {
+            return "";
+        }
+
+        public static int BuscarValorVector()
+        {
+            return 0;
+        }
+
+        public static int Menu()
+        {
+            return 0;
+        }
+    }
+}
