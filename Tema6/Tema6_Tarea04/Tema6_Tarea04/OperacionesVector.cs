13c1ff2079a0c2a06a5d7641084d7899a38ebffe
Manuel MR <manuel.monred@gmail.com>
2026-01-17 14:45:39 +0100

    Tarea 4 - Vector al revés - CONSOLA

--- Tema6/Tema6_Tarea04/Tema6_Tarea04/OperacionesVector.cs
+++ Tema6/Tema6_Tarea04/Tema6_Tarea04/OperacionesVector.cs
@@ -8,29 +8,83 @@ namespace Tema6_Tarea04
 {
     public class OperacionesVector
     {
-        public static int LeerNumero()
+        public static int LeerNumero(string mensaje)
         {
+            int num;
+            bool esCorrecto;
 
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\nIntroduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
         }
 
-        public static void LeerVector()
+        public static void LeerVector(int[] vector)
         {
+            int tamano = vector.Length;
 
+            for (int i = 0; i < tamano; i++)
+            {
+                vector[i] = LeerNumero($"Introduce un valor ({i + 1} de {tamano}): ");
+            }
         }
 
-        public static string GenerarTextoVector()
+        public static string GenerarTextoVector(int[] vector)
         {
+            string texto = "═> Elementos del vector:\n";
+            int tamano = vector.Length;
 
+            for (int i = 0; i < tamano; i++)
+            {
+                if (i != tamano - 1)
+                {
+                    texto += $"{vector[i]}, ";
+                }
+                else
+                {
+                    texto += $"{vector[i]}";
+                }
+            }
+
+            return texto;
         }
 
-        public static int GenerarVectorInvertido()
+        public static int[] GenerarVectorInvertido(int[] vector)
         {
+            int[] vectorInvertido = new int[vector.Length];
+            int j = 0;
 
+            for (int i = vector.Length - 1; i >= 0; i--, j++)
+            {
+                vectorInvertido[j] = vector[i];
+            }
+
+            return vectorInvertido;
         }
 
         public static int Menu()
         {
+            int opcion;
 
+            Console.WriteLine("\n   MENÚ\n" +
+                              "\n1. Leer vector." +
+                              "\n2. Mostrar vector." +
+                              "\n3. Mostrar vector al revés." +
+                              "\n4. Salir del programa.");
+
+            opcion = LeerNumero("\nIntroduce una opción del menú: ");
+            Console.WriteLine();
+
+            return opcion;
         }
     }
 }
--- Tema6/Tema6_Tarea04/Tema6_Tarea04/Program.cs
+++ Tema6/Tema6_Tarea04/Tema6_Tarea04/Program.cs
@@ -12,6 +12,26 @@
             int[] numeros = new int[Tamano];
             int opcion;
 
+            do
+            {
+                opcion = OperacionesVector.Menu();
+
+                switch (opcion)
+                {
+                    case 1:
+                        OperacionesVector.LeerVector(numeros);
+                        break;
+                    case 2:
+                        Console.WriteLine(OperacionesVector.GenerarTextoVector(numeros));
+                        break;
+                    case 3:
+                        int[] numerosInvertido = OperacionesVector.GenerarVectorInvertido(numeros);
+                        Console.WriteLine(OperacionesVector.GenerarTextoVector(numerosInvertido));
+                        break;
+                }
+
+            } while (opcion != 4);
+
             Console.WriteLine("\n\nFin del programa." +
                               "\nPulsa una tecla para salir...");
             Console.ReadKey();
