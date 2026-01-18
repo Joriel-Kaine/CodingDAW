86d7e5417734e5c03645f2f9906c4a9582bc3bd0
Manuel MR <manuel.monred@gmail.com>
2026-01-18 16:20:30 +0100

    Tarea 6 - Desplazamiento circular a la derecha - CONSOLA

---
+++ Tema6/Tema6_Tarea06/Tema6_Tarea06/OperacionesVector.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6_Tarea06
+{
+    public class OperacionesVector
+    {
+        public static int LeerNumero(string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\nIntroduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+
+        public static void LeerVector(int[] vector)
+        {
+            int tamano = vector.Length;
+
+            for (int i = 0; i < tamano; i++)
+            {
+                vector[i] = LeerNumero($"Introduce un valor ({i + 1} de {tamano}): ");
+            }
+        }
+
+        public static string GenerarTextoVector(int[] vector)
+        {
+            string texto = "Elementos del vector:\n";
+            int tamano = vector.Length;
+
+            for (int i = 0; i < tamano; i++)
+            {
+                if (i < tamano - 1)
+                {
+                    texto += $"{vector[i]}, ";
+                }
+                else
+                {
+                    texto += vector[i];
+                }
+            }
+
+            return texto;
+        }
+
+        public static int Menu()
+        {
+            int opcion;
+
+            Console.WriteLine("\n   MENÚ\n" +
+                              "\n1. Leer vector." +
+                              "\n2. Mostrar vector." +
+                              "\n3. Desplazar valor vector." +
+                              "\n4. Salir del programa.");
+
+            opcion = LeerNumero("\nIntroduce una opción del menú: ");
+            Console.WriteLine();
+
+            return opcion;
+        }
+    }
+}
---
+++ Tema6/Tema6_Tarea06/Tema6_Tarea06/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Tema6_Tarea06
+{
+    internal class Program
+    {
+        const int Tamano = 10;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 6 - Tarea 6: Manuel MR ®\n" +
+                              "═════════════════════════════\n\n");
+
+            int[] numeros = new int[Tamano];
+            int opcion;
+            bool esLeido;
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "\nPulsa una tecla para salir...");
+        }
+    }
+}
