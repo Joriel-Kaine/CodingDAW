1a025bf2830739bf2d31bfe1fad8da9154b5595b
Manuel MR <manuel.monred@gmail.com>
2026-01-19 17:40:43 +0100

    Tarea 8 - Monitor de red - INTEGRADOR 1

---
+++ Tema6/Tema6_Tarea08_Integrador1/Tema6_Tarea08_Integrador1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Tema6_Tarea08_Integrador1
+{
+    internal class Program
+    {
+        const int Tamano = 10;
+        const string texto = "\nDebes introducir valores en el vector.";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 6 - Integrador 1: Manuel MR ®\n" +
+                              "══════════════════════════════════\n\n");
+
+            int opcion;
+            bool esLeido = false;
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "\nPulsa una tecla para salir...");
+        }
+    }
+}
