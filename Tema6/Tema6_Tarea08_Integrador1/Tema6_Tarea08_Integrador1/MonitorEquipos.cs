6a9193fa90139462d21c2c9cc991e29c13574277
Manuel MR <manuel.monred@gmail.com>
2026-01-20 18:29:22 +0100

    Tarea 8 - Monitor de red - INTEGRADOR 1

--- Tema6/Tema6_Tarea08_Integrador1/Tema6_Tarea08_Integrador1/MonitorEquipos.cs
+++ Tema6/Tema6_Tarea08_Integrador1/Tema6_Tarea08_Integrador1/MonitorEquipos.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,6 +27,58 @@ namespace Tema6_Tarea08_Integrador1
             } while (!esCorrecto);
 
             return num;
+        }
+
+        public static void LeerDatosRed(string[] IP, int[] latencia)
+        {
+            int tamano = IP.Length;
+
+            for (int i = 0; i < tamano; i++)
+            {
+                do
+                {
+                    Console.Write($"Introduce la IP del equipo ({i + 1} de {tamano}): ");
+                    IP[i] = Console.ReadLine();
+
+                    if (IP[i] is null || IP[i] == "")
+                    {
+                        Console.WriteLine("La IP no puede estar vacía.");
+                    }
+
+                } while (IP[i] is null || IP[i] == "");
+
+
+                do
+                {
+                    latencia[i] = LeerNumero($"Introduce la latencia ({i + 1} de {tamano}): ");
+
+                    if (latencia[i] < 0)
+                    {
+                        Console.WriteLine("\nLa latencia debe ser superior a 0.");
+                    }
+
+                } while (latencia[i] < 0);
+            }
+        }
+
+        public static string ObtenerListadoRed(string[] IP, int[] latencia)
+        {
+            string texto = "Listado de direcciones IP y latencias:\n";
+            int tamano = IP.Length;
+
+            for (int i = 0; i < tamano; i++)
+            {
+                if (i < tamano - 1)
+                {
+                    texto += $"{IP[i]} - {latencia[i]}ms\n";
+                }
+                else
+                {
+                    texto += $"{IP[i]} - {latencia[i]}ms";
+                }
+            }
+
+            return texto;
         }
     }
 }
--- Tema6/Tema6_Tarea08_Integrador1/Tema6_Tarea08_Integrador1/Program.cs
+++ Tema6/Tema6_Tarea08_Integrador1/Tema6_Tarea08_Integrador1/Program.cs
@@ -10,6 +10,14 @@
             Console.WriteLine("\nTema 6 - Integrador 1: Manuel MR ®\n" +
                               "══════════════════════════════════\n\n");
 
+            string[] IP = new string[Tamano];
+            int[] latencia = new int[Tamano];
+
+            string[] IP2 = { "196.168.0.1", "196.168.12.15", "196.168.82.28",
+                             "196.168.55.10", "196.168.18.69", "196.168.47.13",
+                             "196.168.8.2", "196.168.90.5", "196.168.1.11", "196.168.6.3"};
+            int[] latencia2 = { 56, 8, 44, 12, 25, 10, 68, 20, 15, 2 };
+
             int opcion;
             bool esLeido = false;
 
