4df2489570bab82f186cab30578c32bdb25a8c40
Manuel MR <manuel.monred@gmail.com>
2026-01-23 20:23:48 +0100

    Tarea 10 - Copiar y mover pares a otra lista - CONSOLA

---
+++ Tema6/Tema6_Tarea10/Tema6_Tarea10/OperacionesList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6_Tarea10
+{
+    public class OperacionesList
+    {
+        public static int LeerNumero(string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\nIntroduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+    }
+}
---
+++ Tema6/Tema6_Tarea10/Tema6_Tarea10/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tema6_Tarea10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 6 - Tarea 10: Manuel MR ®\n" +
+                              "══════════════════════════════\n\n");
+
+
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "\nPulsa una tecla para salir...");
+        }
+    }
+}
