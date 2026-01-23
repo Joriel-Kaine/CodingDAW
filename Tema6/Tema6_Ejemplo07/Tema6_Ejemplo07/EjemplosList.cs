22967a5e86813b946a14147bc4d5a20084fdb7d5
Manuel MR <manuel.monred@gmail.com>
2026-01-23 19:21:36 +0100

    Ejemplo 7 - Ejemplos con List - CONSOLA

---
+++ Tema6/Tema6_Ejemplo07/Tema6_Ejemplo07/EjemplosList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6_Ejemplo07
+{
+    public class EjemplosList
+    {
+        public static int LeerNumero(string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\nIntroduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+            // Continuamos pidiendo el número mientras no sea un entero.
+
+            return num;
+        }
+
+        // Función a la cual le pasamos el número de elementos de una lista y los leemos, creándola.
+        public static List<int> LeerLista(int numElementos)
+        {
+            List<int> listaNumeros = new();
+
+            for (int i = 0; i < numElementos; i++)
+            {
+                int num = LeerNumero($"Introduce el número {i}: ");
+
+                listaNumeros.Add(num);
+            }
+
+            return listaNumeros;
+        }
+
+        /*
+         * Función a la cual le pasamos dos listas y crea una tercera
+         * con la suma posición a posición de las otras dos.
+         */
+        public static List<int> SumarListas(List<int> lista1, List<int> lista2)
+        {
+            List<int> listaSuma = new();
+
+            for (int i = 0; i < lista1.Count; i++)
+            {
+                int suma = lista1[i] + lista2[i];
+
+                listaSuma.Add(suma);
+            }
+
+            return listaSuma;
+        }
+
+        public static string MostrarLista(List<int> lista)
+        {
+            string texto = "";
+
+            foreach (int num in lista)
+            {
+                texto += $"{num.ToString()} ";
+            }
+
+            return texto;
+        }
+    }
+}
---
+++ Tema6/Tema6_Ejemplo07/Tema6_Ejemplo07/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Tema6_Ejemplo07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 6 - Ejemplo 7: Manuel MR ®\n" +
+                              "═══════════════════════════════\n\n");
+
+            int numElementos = EjemplosList.LeerNumero("Introduce el número de elementos de las listas: ");
+
+            Console.WriteLine("\nIntroducimos la lista 1: ");
+            List<int> listaNumeros1 = EjemplosList.LeerLista(numElementos);
+            Console.WriteLine("\nIntroducimos la lista 2: ");
+            List<int> listaNumeros2 = EjemplosList.LeerLista(numElementos);
+
+            List<int> listaSuma = EjemplosList.SumarListas(listaNumeros1, listaNumeros2);
+
+            Console.WriteLine("Lista 1:");
+            Console.WriteLine(EjemplosList.MostrarLista(listaNumeros1));
+
+            Console.WriteLine("Lista 2:");
+            Console.WriteLine(EjemplosList.MostrarLista(listaNumeros2));
+
+            Console.WriteLine("Lista suma:");
+            Console.WriteLine(EjemplosList.MostrarLista(listaSuma));
+
+            Console.WriteLine("\n\nPulsa una tecla para salir");
+            Console.ReadKey();
+        }
+    }
+}
