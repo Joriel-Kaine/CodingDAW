e4ac04cd57bf01ec23f16d7ed906266be1ef44e4
Manuel MR <manuel.monred@gmail.com>
2026-01-24 12:15:36 +0100

    Tarea 10 - Copiar y mover pares a otra lista - CONSOLA

--- Tema6/Tema6_Tarea10/Tema6_Tarea10/OperacionesList.cs
+++ Tema6/Tema6_Tarea10/Tema6_Tarea10/OperacionesList.cs
@@ -27,5 +27,37 @@ namespace Tema6_Tarea10
 
             return num;
         }
+        /*
+        public static string MostrarListaJoin(List<int> lista)
+        {
+            return string.Join(" ", lista);
+        }
+        */
+        public static string MostrarLista(List<int> lista)
+        {
+            string texto = "";
+
+            foreach (int num in lista)
+            {
+                texto += $"{num.ToString()} ";
+            }
+
+            return texto;
+        }
+
+        public static List<int> LeerLista(int numElementos)
+        {
+            List<int> listaNumeros = new();
+            int num;
+
+            for (int i = 0; i < numElementos; i++)
+            {
+                num = LeerNumero($"Introduce un número ({i + 1} de {numElementos}): ");
+
+                listaNumeros.Add(num);
+            }
+
+            return listaNumeros;
+        }
     }
 }
