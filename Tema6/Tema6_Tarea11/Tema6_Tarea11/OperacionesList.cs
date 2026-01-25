9d1178185295b240a6c1379683ce9b00d721b026
Manuel MR <manuel.monred@gmail.com>
2026-01-25 16:46:27 +0100

    Tarea 11 - Potencias elemento a elemento - CONSOLA

---
+++ Tema6/Tema6_Tarea11/Tema6_Tarea11/OperacionesList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6_Tarea11
+{
+    public class OperacionesList
+    {
+        public static int LeerNumero(string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\nIntroduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+
+        public static void LeerLista(List<int> listaBase, List<int> listaExponente)
+        {
+            int bbase, exponente, posicion = 0;
+            string respuesta = "";
+
+            do
+            {
+
+
+            } while (respuesta == "S" || respuesta == "s");
+        }
+
+        public static 
+    }
+}
---
+++ Tema6/Tema6_Tarea11/Tema6_Tarea11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tema6_Tarea11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 6 - Tarea 11: Manuel MR ®\n" +
+                              "══════════════════════════════\n\n");
+
+
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "\nPulsa una tecla para salir...");
+        }
+    }
+}
