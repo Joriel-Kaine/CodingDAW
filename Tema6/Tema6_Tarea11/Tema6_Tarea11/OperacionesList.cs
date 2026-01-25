c02045100753e59cd25bcdd43b1770d43b1722cb
Manuel MR <manuel.monred@gmail.com>
2026-01-26 00:29:53 +0100

    Tarea 11 - Potencias elemento a elemento - CONSOLA

--- Tema6/Tema6_Tarea11/Tema6_Tarea11/OperacionesList.cs
+++ Tema6/Tema6_Tarea11/Tema6_Tarea11/OperacionesList.cs
@@ -28,18 +28,94 @@ namespace Tema6_Tarea11
             return num;
         }
 
-        public static void LeerLista(List<int> listaBase, List<int> listaExponente)
+        public static void LeerListaBase(List<int> listaBase)
         {
-            int bbase, exponente, posicion = 0;
+            int bbase;
             string respuesta = "";
 
             do
             {
+                bbase = LeerNumero($"Introduce un número (posición {listaBase.Count} de la lista): ");
 
+                listaBase.Add(bbase);
 
-            } while (respuesta == "S" || respuesta == "s");
+                Console.Write("¿Quieres introducir otro valor? (S/N): ");
+                respuesta = Console.ReadLine();
+
+            } while (respuesta.ToUpper() == "S");
         }
 
-        public static 
+        public static void LeerListaExponente(List<int> listaExponente)
+        {
+            int exponente;
+            string respuesta = "";
+
+            do
+            {
+                exponente = LeerNumero($"Introduce un número (posición {listaExponente.Count} de la lista): ");
+
+                listaExponente.Add(exponente);
+
+                Console.Write("¿Quieres introducir otro valor? (S/N): ");
+                respuesta = Console.ReadLine();
+
+            } while (respuesta.ToUpper() == "S");
+        }
+
+        private static int Potencia(int bbase, int exponente)
+        {
+            int potencia = 1;
+
+            for (int i = 0; i < exponente; i++)
+            {
+                potencia *= bbase;
+            }
+
+            return potencia;
+        }
+
+        public static void ListaPotencia(List<int> listaBase, List<int> listaExponente, List<int> listaPotencia)
+        {
+            int menor = listaBase.Count;
+
+            if (menor > listaExponente.Count)
+            {
+                menor = listaExponente.Count;
+            }
+
+            for (int i = 0; i < menor; i++)
+            {
+                listaPotencia.Add(Potencia(listaBase[i], listaExponente[i]));
+            }
+        }
+
+        public static string MostrarLista(List<int> lista)
+        {
+            string texto = "";
+
+            foreach (int num in lista)
+            {
+                texto += $"{num.ToString()} ";
+            }
+
+            return texto;
+        }
+
+        public static int Menu()
+        {
+            int opcion;
+
+            Console.WriteLine("\n■ Potencias ■" +
+                              "\n1. Leer bases." +
+                              "\n2. Leer exponentes." +
+                              "\n3. Calcular potencias." +
+                              "\n4. Mostrar listas." +
+                              "\n5. Salir del programa.");
+
+            opcion = LeerNumero("\nIntroduce una opción del menú: ");
+            Console.WriteLine();
+
+            return opcion;
+        }
     }
 }
--- Tema6/Tema6_Tarea11/Tema6_Tarea11/Program.cs
+++ Tema6/Tema6_Tarea11/Tema6_Tarea11/Program.cs
@@ -2,12 +2,60 @@
 {
     internal class Program
     {
+        const string texto = "Debes introducir valores en la lista.";
+
         static void Main(string[] args)
         {
             Console.WriteLine("\nTema 6 - Tarea 11: Manuel MR ®\n" +
                               "══════════════════════════════\n\n");
 
+            List<int> listaBase = new(), listaExponente = new(), listaPotencia = new();
+            int opcion;
+            bool esBaseLeida = false, esExponenteLeido = false;
 
+            do
+            {
+                opcion = OperacionesList.Menu();
+
+                switch (opcion)
+                {
+                    case 1:
+                        OperacionesList.LeerListaBase(listaBase);
+                        esBaseLeida = true;
+                        break;
+                    case 2:
+                        OperacionesList.LeerListaExponente(listaExponente);
+                        esExponenteLeido = true;
+                        break;
+                    case 3:
+                        if (!esBaseLeida || !esExponenteLeido)
+                        {
+                            Console.WriteLine(texto);
+                        }
+                        else
+                        {
+                            listaPotencia.Clear();
+                            OperacionesList.ListaPotencia(listaBase, listaExponente, listaPotencia);
+                        }
+                        break;
+                    case 4:
+                        if (!esBaseLeida || !esExponenteLeido)
+                        {
+                            Console.WriteLine(texto);
+                        }
+                        else
+                        {
+                            Console.WriteLine($"\nLista de valores para la base:\n" +
+                                              $"{OperacionesList.MostrarLista(listaBase)}" +
+                                              $"\nLista de valores para el exponente:\n" +
+                                              $"{OperacionesList.MostrarLista(listaExponente)}" +
+                                              $"\nLista de valores para la potencia (base ^ exponente):\n" +
+                                              $"{OperacionesList.MostrarLista(listaPotencia)}");
+                        }
+                        break;
+                }
+
+            } while (opcion != 5);
 
             Console.WriteLine("\n\nFin del programa." +
                               "\nPulsa una tecla para salir...");
