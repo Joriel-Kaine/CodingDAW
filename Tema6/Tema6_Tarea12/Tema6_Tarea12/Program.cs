52dcea2de2f7cc0970bf11821752ea480e0f1460
Manuel MR <manuel.monred@gmail.com>
2026-01-26 03:22:01 +0100

    Tarea 12 - Intercalar dos listas ordenadas - CONSOLA

---
+++ Tema6/Tema6_Tarea12/Tema6_Tarea12/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Tema6_Tarea12
+{
+    internal class Program
+    {
+        const string texto = "\nDebes introducir valores en la lista.",
+                     titulo = "\nTema 6 - Tarea 12: Manuel MR ®\n" +
+                              "══════════════════════════════\n\n";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(titulo);
+
+
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
