1e96e3c4d2bac630f9ab5ae44201803c166cf84c
Manuel MR <manuel.monred@gmail.com>
2026-02-03 03:35:40 +0100

    Tarea 14 - Lotería primitiva - CONSOLA

---
+++ Tema6/Tema6_Tarea14_Integrador2/Tema6_Tarea14_Integrador2/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Tema6_Tarea14_Integrador2
+{
+    internal class Program
+    {
+        const string texto = "Debes introducir valores en la lista.",
+                     titulo = "\nTema 6 - Tarea 14: Manuel MR ®\n" +
+                              "══════════════════════════════\n\n";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(titulo);
+
+
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "\nPulsa una tecla para salir...");
+        }
+    }
+}
