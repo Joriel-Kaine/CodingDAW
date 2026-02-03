12c3b8838fdd173df3dc1d0c053df8e903477d07
Manuel MR <manuel.monred@gmail.com>
2026-02-03 03:29:57 +0100

    Tarea 13 - Insertar palabra orden alfabético - CONSOLA

---
+++ Tema6/Tema6_Tarea13/Tema6_Tarea13/OperacionesList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6_Tarea13
+{
+    public class OperacionesList
+    {
+        public static int LeerNumero(string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("Introduce valores numéricos válidos.");
+                }
+
+            } while (!esCorrecto);
+
+            return num;
+        }
+    }
+}
---
+++ Tema6/Tema6_Tarea13/Tema6_Tarea13/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Tema6_Tarea13
+{
+    internal class Program
+    {
+        const string texto = "Debes introducir valores en la lista",
+                     titulo = "\nTema 6 - Tarea 13: Manuel MR ®\n" +
+                              "══════════════════════════════\n\n";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(titulo);
+
+            int opcion;
+
+            do
+            {
+                opcion = 0;
+
+            } while (opcion != 0);
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
