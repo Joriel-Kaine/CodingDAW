4945c25051afb1472e6a3bca588d93db28c98949
Manuel MR <manuel.monred@gmail.com>
2026-02-08 14:43:57 +0100

    Tarea 14 - Lotería primitiva - INTEGRADOR 2

---
+++ Tema6/Tema6_Tarea14_Integrador2/Tema6_Tarea14_Integrador2/LoteriaPrimitiva.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema6_Tarea14_Integrador2
+{
+    public class LoteriaPrimitiva
+    {
+        const int CantidadNum = 6;
+
+        public static int LeerEntero(string mensaje)
+        {
+            int num;
+            bool esCorrecto;
+
+            do
+            {
+                Console.Write(mensaje);
+                esCorrecto = int.TryParse(Console.ReadLine(), out num);
+
+                if (!esCorrecto)
+                {
+                    Console.WriteLine("\nDebes introducir valores numéricos válidos.");
+                }
+
+                if (esCorrecto && (num < 1 || num > 49))
+                {
+                    Console.WriteLine("\nLos números deben estar comprendidos entre 1 y 49.");
+                }
+
+            } while (!esCorrecto || (num < 1 || num > 49));
+
+            return num;
+        }
+
+        public static void GenerarNumeros(List<int> numeros, Random random)
+        {
+            int num;
+
+            numeros.Clear();
+
+            for (int i = 0; i < CantidadNum; i++)
+            {
+                num = random.Next(1, 50);
+
+                if (!numeros.Contains(num))
+                {
+                    numeros.Add(num);
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            numeros.Sort();
+        }
+
+        public static void LeerNumeros(List<int> numeros)
+        {
+            int num;
+
+            numeros.Clear();
+
+            for (int i = 0; i < CantidadNum; i++)
+            {
+                num = LeerEntero($"Introduce el número para la apuesta ({i + 1} de {CantidadNum}): ");
+
+                if (!numeros.Contains(num))
+                {
+                    numeros.Add(num);
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            numeros.Sort();
+        }
+
+        public static int ComprobarAciertos(List<int> numSorteo, List<int> numApuesta)
+        {
+            int aciertos = 0;
+
+            foreach (int num in numSorteo)
+            {
+                if (numApuesta.Contains(num))
+                {
+                    aciertos++;
+                }
+            }
+
+            return aciertos;
+        }
+
+        public static string MostrarApuestas(List<int> numeros)
+        {
+            string texto = "";
+
+            foreach (int num in numeros)
+            {
+                texto += $"{num} - ";
+            }
+
+            if (texto != "")
+            {
+                texto = texto.Remove(texto.Length - 3);
+            }
+
+            return texto;
+        }
+
+        public static int Menu()
+        {
+            int opcion;
+
+            Console.WriteLine("\nLotería Primitiva\n" +
+                              "\n1. Generar sorteo." +
+                              "\n2. Apuesta manual." +
+                              "\n3. Apuesta automática." +
+                              "\n4. Comprobar aciertos." +
+                              "\n5. Mostrar apuestas." +
+                              "\n6. Salir del programa.");
+
+            opcion = LeerEntero("\nIntroduce una opción del menú: ");
+            Console.WriteLine();
+
+            return opcion;
+        }
+    }
+}
--- Tema6/Tema6_Tarea14_Integrador2/Tema6_Tarea14_Integrador2/Program.cs
+++ Tema6/Tema6_Tarea14_Integrador2/Tema6_Tarea14_Integrador2/Program.cs
@@ -2,19 +2,121 @@
 {
     internal class Program
     {
-        const string texto = "Debes introducir valores en la lista.",
-                     titulo = "\nTema 6 - Tarea 14: Manuel MR ®\n" +
+        const string Apuesta = "\nYa hay una apuesta realizada lista para ser comprobada.",
+                     Texto = "\nDebe haber primero una apuesta generada.",
+                     Titulo = "\nTema 6 - Tarea 14: Manuel MR ®\n" +
                               "══════════════════════════════\n\n";
 
         static void Main(string[] args)
         {
-            Console.WriteLine(titulo);
+            Console.WriteLine(Titulo);
 
+            List<int> numSorteo = new(), apuestaManual = new(), apuestaAuto = new();
+            int opcion;
+            Random random = new();
+            bool esManual = false, esAuto = false, esSorteo = false;
 
+            do
+            {
+                opcion = LoteriaPrimitiva.Menu();
 
+                switch (opcion)
+                {
+                    case 1:
+                        if (!esSorteo)
+                        {
+                            LoteriaPrimitiva.GenerarNumeros(numSorteo, random);
+                            esSorteo = true;
+                            Console.WriteLine("\nSorteo generado de manera aleatoria.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("\nYa hay un sorteo activo.");
+                        }
+                        break;
+                    case 2:
+                        if (!esAuto && !esManual)
+                        {
+                            LoteriaPrimitiva.LeerNumeros(apuestaManual);
+                            esManual = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine(Apuesta);
+                        }
+                        break;
+                    case 3:
+                        if (!esManual && !esAuto)
+                        {
+                            LoteriaPrimitiva.GenerarNumeros(apuestaAuto, random);
+                            esAuto = true;
+                            Console.WriteLine("\nApuesta generada de manera aleatoria.");
+                        }
+                        else
+                        {
+                            Console.WriteLine(Apuesta);
+                        }
+                        break;
+                    case 4:
+                        if (esManual && esSorteo)
+                        {
+                            Console.WriteLine($"\nEl número de aciertos de la lotería es: " +
+                                              $"{LoteriaPrimitiva.ComprobarAciertos(numSorteo, apuestaManual)}");
+                        }
+                        else if (esAuto && esSorteo)
+                        {
+                            Console.WriteLine($"\nEl número de aciertos de la lotería es: " +
+                                              $"{LoteriaPrimitiva.ComprobarAciertos(numSorteo, apuestaAuto)}");
+                        }
+                        else
+                        {
+                            Console.WriteLine(Texto);
+                        }
+                        break;
+                    case 5:
+                        if (esManual && esSorteo)
+                        {
+                            Console.WriteLine($"\nLos números del sorteo son:\n" +
+                                              $"═>{LoteriaPrimitiva.MostrarApuestas(numSorteo)}\n" +
+                                              $"\nLos números de la apuesta manual son:\n" +
+                                              $"═>{LoteriaPrimitiva.MostrarApuestas(apuestaManual)}\n");
+                            
+                            esSorteo = false; esManual = false; esAuto = false;
+                        }
+                        else if (esAuto && esSorteo)
+                        {
+                            Console.WriteLine($"\nLos números del sorteo son:\n" +
+                                              $"═>{LoteriaPrimitiva.MostrarApuestas(numSorteo)}\n" +
+                                              $"\nLos números de la apuesta automática son:\n" +
+                                              $"═>{LoteriaPrimitiva.MostrarApuestas(apuestaAuto)}\n");
+
+                            esSorteo = false; esManual = false; esAuto = false;
+                        }
+                        else
+                        {
+                            Console.WriteLine(Texto);
+                        }
+                        break;
+                }
+
+                if (opcion != 6)
+                {
+                    Console.WriteLine("\nPulsa una tecla para volver al menú...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Console.WriteLine(Titulo);
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine(Titulo);
+                }
+
+            } while (opcion != 6);
 
             Console.WriteLine("\n\nFin del programa." +
                               "\nPulsa una tecla para salir...");
+            Console.ReadKey();
         }
     }
 }
