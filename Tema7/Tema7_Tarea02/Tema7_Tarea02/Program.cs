ad26d5550636eb894a3b1b2b9d48198a95e61777
Manuel MR <manuel.monred@gmail.com>
2026-02-11 04:26:12 +0100

    Tarea 2 - Lista de fechas - CONSOLA

---
+++ Tema7/Tema7_Tarea02/Tema7_Tarea02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tema7_Tarea02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
