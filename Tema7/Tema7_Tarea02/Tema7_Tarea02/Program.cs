9896bf163ca2e2212c9decbb734205d1ac974557
Manuel MR <manuel.monred@gmail.com>
2026-02-11 13:18:24 +0100

    Tarea 2 - Lista de fechas - CONSOLA

--- Tema7/Tema7_Tarea02/Tema7_Tarea02/Program.cs
+++ Tema7/Tema7_Tarea02/Tema7_Tarea02/Program.cs
@@ -2,9 +2,17 @@
 {
     internal class Program
     {
+        const string Titulo = "\nTema 7 - Tarea 2: Manuel MR ®\n" +
+                              "═════════════════════════════\n\n";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Titulo);
+
+
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "\nPulsa una tecla para finalizar...");
         }
     }
 }
