d67eb82144d98d8afbcddec47d9644e6ddfef252
Manuel MR <manuel.monred@gmail.com>
2026-02-13 01:55:47 +0100

    Tarea 3 - Lista de alumnos - CONSOLA

---
+++ Tema7/Tema7_Tarea03/Tema7_Tarea03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Tema7_Tarea03
+{
+    internal class Program
+    {
+        const string Titulo = "\nTema 7 - Tarea 3: Manuel MR ®\n" +
+                              "═════════════════════════════\n\n";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Titulo);
+
+
+
+            Console.WriteLine("\n\nFin del programa." +
+                              "\nPulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
