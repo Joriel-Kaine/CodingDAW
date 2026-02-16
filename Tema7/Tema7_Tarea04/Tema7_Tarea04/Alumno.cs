3c4c76614a60d0b9c8ee62746c6239562a3ab73c
Manuel MR <manuel.monred@gmail.com>
2026-02-16 10:40:11 +0100

    Tarea 4 - Lista de alumnos - FORMS

--- Tema7/Tema7_Tarea04/Tema7_Tarea04/Alumno.cs
+++ Tema7/Tema7_Tarea04/Tema7_Tarea04/Alumno.cs
@@ -8,6 +8,94 @@ namespace Tema7_Tarea04
 {
     public class Alumno
     {
+        // Campo.
+        public string Nombre { get; set; }
+        private int _edad;
+        private List<double> _notas = new();
 
+        // Propiedad.
+        public int Edad
+        {
+            get { return _edad; }
+            set
+            {
+                if (value >= 0 && value <= 100)
+                {
+                    _edad = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("La edad debe estar entre 0 y 100.");
+                }
+            }
+        }
+
+        // Constructor.
+        public Alumno()
+        {
+            Nombre = "";
+            Edad = 0;
+        }
+
+        public Alumno(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
+        }
+
+        // Método.
+        public void Birthday()
+        {
+            Edad++;
+        }
+
+        public void AddNota(double nota)
+        {
+            if (nota >= 0 && nota <= 10)
+            {
+                _notas.Add(nota);
+            }
+        }
+
+        private string MostrarNotas()
+        {
+            string texto;
+
+            if (_notas.Count > 0)
+            {
+                texto = "Notas: ";
+
+                for (int i = 0; i < _notas.Count; i++)
+                {
+                    if (i != _notas.Count - 1)
+                    {
+                        texto += $"{_notas[i]} - ";
+                    }
+                    else
+                    {
+                        texto += _notas[i];
+                    }
+                    
+                }
+            }
+            else
+            {
+                texto = "Alumno sin notas.";
+            }
+
+            return texto;
+        }
+
+        public string MostrarDatos()
+        {
+            string texto;
+
+            texto = $"\nDatos del alumno:\n" +
+                    $"Nombre: {Nombre}\n" +
+                    $"Edad: {Edad}\n" +
+                    MostrarNotas();
+
+            return texto;
+        }
     }
 }
--- Tema7/Tema7_Tarea04/Tema7_Tarea04/FormAlumno.Designer.cs
+++ Tema7/Tema7_Tarea04/Tema7_Tarea04/FormAlumno.Designer.cs
@@ -31,7 +31,12 @@
             components = new System.ComponentModel.Container();
             lblTitulo = new Label();
             groupBox1 = new GroupBox();
+            btnLeerAlumno = new Button();
             contextMenuStrip1 = new ContextMenuStrip(components);
+            btnAddNota = new Button();
+            btnBirthday = new Button();
+            btnMostrarAlumno = new Button();
+            groupBox1.SuspendLayout();
             SuspendLayout();
             // 
             // lblTitulo
@@ -48,14 +53,31 @@
             // groupBox1
             // 
             groupBox1.BackColor = Color.LightGray;
+            groupBox1.Controls.Add(btnMostrarAlumno);
+            groupBox1.Controls.Add(btnBirthday);
+            groupBox1.Controls.Add(btnAddNota);
+            groupBox1.Controls.Add(btnLeerAlumno);
             groupBox1.Font = new Font("Maiandra GD", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
             groupBox1.ForeColor = Color.MidnightBlue;
             groupBox1.Location = new Point(96, 164);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(900, 470);
+            groupBox1.Size = new Size(1000, 380);
             groupBox1.TabIndex = 1;
             groupBox1.TabStop = false;
             groupBox1.Text = "Alumnos";
+            // 
+            // btnLeerAlumno
+            // 
+            btnLeerAlumno.BackColor = Color.LightBlue;
+            btnLeerAlumno.FlatAppearance.BorderColor = Color.SteelBlue;
+            btnLeerAlumno.FlatAppearance.BorderSize = 3;
+            btnLeerAlumno.FlatStyle = FlatStyle.Flat;
+            btnLeerAlumno.Location = new Point(78, 67);
+            btnLeerAlumno.Name = "btnLeerAlumno";
+            btnLeerAlumno.Size = new Size(360, 80);
+            btnLeerAlumno.TabIndex = 0;
+            btnLeerAlumno.Text = "Introducir Alumno";
+            btnLeerAlumno.UseVisualStyleBackColor = false;
             // 
             // contextMenuStrip1
             // 
@@ -63,16 +85,56 @@
             contextMenuStrip1.Name = "contextMenuStrip1";
             contextMenuStrip1.Size = new Size(61, 4);
             // 
-            // Form1
+            // btnAddNota
+            // 
+            btnAddNota.BackColor = Color.LightBlue;
+            btnAddNota.FlatAppearance.BorderColor = Color.SteelBlue;
+            btnAddNota.FlatAppearance.BorderSize = 3;
+            btnAddNota.FlatStyle = FlatStyle.Flat;
+            btnAddNota.Location = new Point(551, 67);
+            btnAddNota.Name = "btnAddNota";
+            btnAddNota.Size = new Size(360, 80);
+            btnAddNota.TabIndex = 1;
+            btnAddNota.Text = "Añadir Nota";
+            btnAddNota.UseVisualStyleBackColor = false;
+            // 
+            // btnBirthday
+            // 
+            btnBirthday.BackColor = Color.LightBlue;
+            btnBirthday.FlatAppearance.BorderColor = Color.SteelBlue;
+            btnBirthday.FlatAppearance.BorderSize = 3;
+            btnBirthday.FlatStyle = FlatStyle.Flat;
+            btnBirthday.Location = new Point(78, 207);
+            btnBirthday.Name = "btnBirthday";
+            btnBirthday.Size = new Size(360, 100);
+            btnBirthday.TabIndex = 2;
+            btnBirthday.Text = "Cumpleaños de un Alumno";
+            btnBirthday.UseVisualStyleBackColor = false;
+            // 
+            // btnMostrarAlumno
+            // 
+            btnMostrarAlumno.BackColor = Color.LightBlue;
+            btnMostrarAlumno.FlatAppearance.BorderColor = Color.SteelBlue;
+            btnMostrarAlumno.FlatAppearance.BorderSize = 3;
+            btnMostrarAlumno.FlatStyle = FlatStyle.Flat;
+            btnMostrarAlumno.Location = new Point(551, 207);
+            btnMostrarAlumno.Name = "btnMostrarAlumno";
+            btnMostrarAlumno.Size = new Size(360, 100);
+            btnMostrarAlumno.TabIndex = 3;
+            btnMostrarAlumno.Text = "Mostrar Datos de un Alumno";
+            btnMostrarAlumno.UseVisualStyleBackColor = false;
+            // 
+            // FormAlumno
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.LightGray;
-            ClientSize = new Size(1578, 1144);
+            ClientSize = new Size(1658, 1144);
             Controls.Add(groupBox1);
             Controls.Add(lblTitulo);
-            Name = "Form1";
+            Name = "FormAlumno";
             Text = "Tema 7 - Tarea 4: Manuel MR ®";
+            groupBox1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -82,5 +144,9 @@
         private Label lblTitulo;
         private GroupBox groupBox1;
         private ContextMenuStrip contextMenuStrip1;
+        private Button btnLeerAlumno;
+        private Button btnMostrarAlumno;
+        private Button btnBirthday;
+        private Button btnAddNota;
     }
 }
