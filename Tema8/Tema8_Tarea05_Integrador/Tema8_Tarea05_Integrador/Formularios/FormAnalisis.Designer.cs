3d73b65b0d58fce0e22f906f22dec5821867ce2b
Manuel MR <manuel.monred@gmail.com>
2026-04-16 18:11:43 +0200

    Tarea 5 - Gestión de proyectos profesionales - INTEGRADOR

--- Tema8/Tema8_Tarea05_Integrador/Tema8_Tarea05_Integrador/Formularios/FormAnalisis.Designer.cs
+++ Tema8/Tema8_Tarea05_Integrador/Tema8_Tarea05_Integrador/Formularios/FormAnalisis.Designer.cs
@@ -28,18 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
+            lstAnalisis = new ListBox();
+            btnTotalProfesionalesProyectos = new Button();
             SuspendLayout();
+            // 
+            // lstAnalisis
+            // 
+            lstAnalisis.BackColor = Color.LightSteelBlue;
+            lstAnalisis.BorderStyle = BorderStyle.FixedSingle;
+            lstAnalisis.Font = new Font("Maiandra GD", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lstAnalisis.FormattingEnabled = true;
+            lstAnalisis.ItemHeight = 29;
+            lstAnalisis.Location = new Point(85, 401);
+            lstAnalisis.Name = "lstAnalisis";
+            lstAnalisis.Size = new Size(1200, 437);
+            lstAnalisis.TabIndex = 8;
+            // 
+            // btnTotalProfesionalesProyectos
+            // 
+            btnTotalProfesionalesProyectos.BackColor = Color.Tan;
+            btnTotalProfesionalesProyectos.FlatAppearance.BorderColor = Color.SteelBlue;
+            btnTotalProfesionalesProyectos.FlatAppearance.BorderSize = 4;
+            btnTotalProfesionalesProyectos.FlatStyle = FlatStyle.Flat;
+            btnTotalProfesionalesProyectos.Font = new Font("Maiandra GD", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnTotalProfesionalesProyectos.Location = new Point(378, 844);
+            btnTotalProfesionalesProyectos.Name = "btnTotalProfesionalesProyectos";
+            btnTotalProfesionalesProyectos.Size = new Size(600, 60);
+            btnTotalProfesionalesProyectos.TabIndex = 9;
+            btnTotalProfesionalesProyectos.Text = "Total de Profesionales y Proyectos";
+            btnTotalProfesionalesProyectos.UseVisualStyleBackColor = false;
             // 
             // FormAnalisis
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1378, 924);
+            Controls.Add(btnTotalProfesionalesProyectos);
+            Controls.Add(lstAnalisis);
             Name = "FormAnalisis";
             Text = "FormAnalisis";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private ListBox lstAnalisis;
+        private Button btnTotalProfesionalesProyectos;
     }
 }
