bf71cdb227ac9d12af4f49757bb70f08de8e8606
Manuel MR <manuel.monred@gmail.com>
2026-04-27 11:05:56 +0200

    Tarea 1 - Persistencia estudiantes en Supabase - CONSOLE

---
+++ Tema9/Tema9_Tarea01/Tema9_Tarea01/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tema9_Tarea01.Data;
+using Tema9_Tarea01.Models;
+using Tema9_Tarea01.Repositories;
+
+namespace Tema9_Tarea01
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("\nTema 9 - Tarea 1: Manuel MR ®\n" +
+                              "═════════════════════════════\n\n");
+
+            var client = await SupabaseClientFactory.GetClientAsync();
+            var repo = new StudentsRepository(client);
+
+            var inserted = await repo.InsertAsync(new Student(null, "Lito Kaine", 38));
+            Console.WriteLine($"Insertado: {inserted?.Id} - {inserted?.Name} - {inserted?.Age}");
+
+            var students = await repo.GetAllAsync();
+            foreach (var student in students)
+            {
+                Console.WriteLine($"{student.Id} - {student.Name} - {student.Age}");
+            }
+
+            Console.WriteLine("Pulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tema9/Tema9_Tarea01/Tema9_Tarea01/Repositories/StudentsRepository.cs
@@ -0,0 +1,32 @@
+﻿using Supabase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tema9_Tarea01.Models;
+
+namespace Tema9_Tarea01.Repositories
+{
+    public class StudentsRepository
+    {
+        private readonly Client _client;
+        public StudentsRepository(Client client)
+        {
+            _client = client;
+        }
+
+        public async Task<List<Student>> GetAllAsync()
+        {
+            var response = await _client.From<Student>().Get();
+
+            return response.Models ?? new List<Student>();
+        }
+
+        public async Task<Student?> InsertAsync(Student student)
+        {
+            var response = await _client.From<Student>().Insert(student);
+            return response.Models?.FirstOrDefault() ?? null;
+        }
+    }
+}
