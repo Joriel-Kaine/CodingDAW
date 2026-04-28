effbbfbbb030eb730246292850750bbb770dcbac
Manuel MR <manuel.monred@gmail.com>
2026-04-28 14:41:47 +0200

    Tarea 2 - Menú tabla estudiantes - CONSOLE

---
+++ Tema9/Tema9_Tarea02/Tema9_Tarea02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tema9_Tarea02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
