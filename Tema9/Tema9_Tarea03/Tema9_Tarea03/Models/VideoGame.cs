8916faa779f44666c82a959b0d84f9d3ed9919c2
Manuel MR <manuel.monred@gmail.com>
2026-05-02 19:19:43 +0200

    Tarea 3 - Menú tabla a elección - CONSOLE

--- Tema9/Tema9_Tarea03/Tema9_Tarea03/Models/VideoGame.cs
+++ Tema9/Tema9_Tarea03/Tema9_Tarea03/Models/VideoGame.cs
@@ -1,13 +1,45 @@
-﻿using System;
+﻿using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tema9_Tarea03.Models
 {
-    public class VideoGame
+    [Table("videogames")]
+    public class VideoGame : BaseModel
     {
+        [PrimaryKey("id", false)]
+        public Guid Id { get; set; }
+        [Column("appid")]   
+        public int AppId { get; set; }
+        [Column("name")]
+        public string Name { get; set; } = "";
+        [Column("genre")]
+        public string Genre { get; set; } = "";
+        [Column("year")]
+        public int Year { get; set; }
+        [Column("rating")]
+        public double Rating { get; set; }
+        [Column("price")]
+        public decimal Price { get; set; }
 
+        public VideoGame() { }
+
+        public VideoGame(int appId, string name, string genre, int year, double rating, decimal price)
+        {
+            AppId = appId;
+            Name = name;
+            Genre = genre;
+            Year = year;
+            Rating = rating;
+            Price = price;
+        }
+
+        public override string ToString()
+            => $"{AppId} | {Name} | {Genre} | {Year} | {Rating} | {Price:C2}";
     }
 }
