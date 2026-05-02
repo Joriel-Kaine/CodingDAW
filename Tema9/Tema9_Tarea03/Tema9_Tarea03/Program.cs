dadb23cc5e077e794edc052a841c31fe711da7d5
Manuel MR <manuel.monred@gmail.com>
2026-05-02 17:40:14 +0200

    Tarea 3 - Menú tabla a elección - CONSOLE

---
+++ Tema9/Tema9_Tarea03/Tema9_Tarea03/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tema9_Tarea03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nTema 9 - Tarea 3: Manuel MR ®\n" +
+                              "═════════════════════════════\n\n");
+
+
+
+            Console.WriteLine("Pulsa una tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Tema9/Tema9_Tarea03/Tema9_Tarea03/Views/ConsoleView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema9_Tarea03.Views
+{
+    public static class ConsoleView
+    {
+
+    }
+}
