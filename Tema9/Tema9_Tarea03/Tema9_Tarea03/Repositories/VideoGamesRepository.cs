237ba239a0fe74b601df892f3bcdd4cf793b824b
Manuel MR <manuel.monred@gmail.com>
2026-05-03 18:47:04 +0200

    Tarea 3 - Menú tabla a elección - CONSOLE

--- Tema9/Tema9_Tarea03/Tema9_Tarea03/Repositories/VideoGamesRepository.cs
+++ Tema9/Tema9_Tarea03/Tema9_Tarea03/Repositories/VideoGamesRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Supabase;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,13 @@ namespace Tema9_Tarea03.Repositories
 {
     public class VideoGamesRepository
     {
+        private readonly Client _client;
+
+        public VideoGamesRepository(Client client)
+        {
+            _client = client;
+        }
+
 
     }
 }
