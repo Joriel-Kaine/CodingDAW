8e41d21d3fdf27681fb5aaec11323bedca8d51a2
Manuel MR <manuel.monred@gmail.com>
2026-05-04 11:09:15 +0200

    Tarea 2 - Menú tabla estudiantes - CONSOLE

--- Tema9/Tema9_Tarea03/Tema9_Tarea03/Repositories/VideoGamesRepository.cs
+++ Tema9/Tema9_Tarea03/Tema9_Tarea03/Repositories/VideoGamesRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Supabase;
+using static Supabase.Postgrest.Constants;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tema9_Tarea03.Models;
 
 namespace Tema9_Tarea03.Repositories
 {
@@ -16,6 +18,34 @@ namespace Tema9_Tarea03.Repositories
             _client = client;
         }
 
+        // Lista de Videojuegos.
+        public async Task<List<VideoGame>> GetAllAsync()
+        {
+            var response = await _client
+                .From<VideoGame>()
+                .Get();
 
+            return response.Models ?? new List<VideoGame>();
+        }
+
+        // Añadir un videojuego.
+        public async Task<VideoGame?> InsertVideoGame(VideoGame videoGame)
+        {
+            var response = await _client
+                .From<VideoGame>()
+                .Insert(videoGame);
+
+            return response.Models?.FirstOrDefault() ?? null;
+        }
+
+        public async Task<VideoGame?> GetByAppIdAsync(int appId)
+        {
+            var response = await _client
+                .From<VideoGame>()
+                .Filter("appid", Operator.Equals, appId)
+                .Single();
+
+            return response;
+        }
     }
 }
