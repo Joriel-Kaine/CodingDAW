7df10f3dae5380502328b751d495a1bcc1e2e5dc
Manuel MR <manuel.monred@gmail.com>
2026-05-14 13:57:59 +0200

    Tarea 5 - Integrador 5 y 6 - FORMS

--- Tema9/Tema9_Tarea05-06_Integrador/Tema9_Tarea05-06_Integrador/Views/FormStudents.cs
+++ Tema9/Tema9_Tarea05-06_Integrador/Tema9_Tarea05-06_Integrador/Views/FormStudents.cs
@@ -1,3 +1,5 @@
+using Tema9_Tarea05_06_Integrador.Controllers;
+
 namespace Tema9_Tarea05_06_Integrador
 {
     public partial class FormStudents : Form
@@ -7,11 +9,95 @@ namespace Tema9_Tarea05_06_Integrador
             InitializeComponent();
         }
 
+
+        // Métodos auxiliares.
+        private static int ReadInteger(string message)
+        {
+            int num;
+            bool isCorrect;
+
+            Console.Write(message);
+            isCorrect = int.TryParse(Console.ReadLine(), out num);
+
+            if (!isCorrect)
+            {
+                throw new FormatException("Introduce valores numéricos válidos.");
+            }
+
+            return num;
+        }
+
+        private static string ReadString(string message, string displayName)
+        {
+            Console.Write(message);
+            var value = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException($"El {displayName} no puede estar vacío.");
+            }
+
+            return value;
+        }
+
+        private static int? ReadNullInteger(string message)
+        {
+            Console.Write(message);
+            var value = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return null;
+            }
+
+            if (!int.TryParse(value, out int num))
+            {
+                throw new FormatException("Introduce valores numéricos válidos.");
+            }
+
+            return num;
+        }
+
+        private static async Task SafeExecuteAsync(Func<Task> action)
+        {
+            try
+            {
+                await action();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Pulsa una tecla para continuar...");
+            Console.ReadKey();
+        }
+
+        private static async Task PrintAllStudents(StudentsController controller)
+        {
+            var students = await controller.GetAsyncAll();
+
+            if (students.Count == 0)
+            {
+                Console.WriteLine("La lista está vacía.");
+                return;
+            }
+
+            foreach (var student in students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+
+
+        // Load del formulario.
         private void FormStudents_Load(object sender, EventArgs e)
         {
 
         }
 
+
+        // Botones.
         private void btnAddStudent_Click(object sender, EventArgs e)
         {
 
